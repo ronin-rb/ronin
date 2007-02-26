5637896241d4549db9eb45f2d3ee70c8f3aec395
postmodern <postmodern.mod3@gmail.com>
2007-02-26 08:19:45 +0000

    Added contexts for 'BufferOverflow', 'FormatString' and 'PlatformExploit' classes.

---
+++ lib/repo/formatstringcontext.rb
@@ -0,0 +1,50 @@
+#
+# Ronin - A decentralized repository for the storage and sharing of computer
+# security advisories, exploits and payloads.
+#
+# Copyright (c) 2007 Hal Brodigan (postmodern at users.sourceforge.net)
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+#
+
+require 'formatstring'
+require 'repo/platformexploitcontext'
+
+module Ronin
+  module Repo
+    class FormatStringContext < PlatformExploitContext
+
+      def initialize(path)
+	super(path)
+      end
+
+      def create_exploit!
+	@exploit = FormatString.new(self.advisory)
+	load_formatstring(@exploit)
+      end
+
+      protected
+
+      def target(product_version,platform,pop_length,address,overwrite,comments=nil)
+	@targets << FormatStringTarget.new(product_version,platform,pop_length,address,overwrite,comments)
+      end
+      
+      def load_formatstring(exploit)
+	load_platformexploit(exploit)
+      end
+
+    end
+  end
+end
