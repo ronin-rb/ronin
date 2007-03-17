adebb2c6150d4a9114f343750aa1cd4601b428a7
postmodern <postmodern.mod3@gmail.com>
2007-03-17 02:24:53 +0000

    * Minor fixes. * Added the license pre-amble to files. * Added some sparse documentation to immed.rb and reg.rb.

--- lib/asm/block.rb
+++ lib/asm/block.rb
@@ -1,3 +1,24 @@
+#
+# Ronin - A decentralized repository for the storage and sharing of computer
+# security advisories, exploits and payloads.
+#
+# Copyright (c) 2007 Hal Brodigan (postmodern at users.sourceforge.net)
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+#
+
 require 'asm/type'
 require 'asm/reg'
 require 'asm/variable'
@@ -46,8 +67,6 @@ module Ronin
       def asm(&block)
 	inline(block(&block))
       end
-
-      protected
 
       def emit(op,*args)
 	@instructions << Instruction.new(op,*args)
@@ -160,6 +179,8 @@ module Ronin
 
 	@functions[name] = Func.new(name,*args,&block)
       end
+
+      protected
 
       def method_missing(sym,*args)
 	return @functions[sym] if @functions.has_key(sym)
