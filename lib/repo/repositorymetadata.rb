c707036fef0b89099b9d602b8c4b4c3f41ebadd0
postmodern <postmodern.mod3@gmail.com>
2007-04-15 03:36:32 +0000

    * Added license header. * Added license attribute to the Repository class.

--- lib/repo/repositorymetadata.rb
+++ lib/repo/repositorymetadata.rb
@@ -1,3 +1,24 @@
+#
+# Ronin - A decentralized repository for the storage and sharing of computer
+# security advisories, exploits and payloads.
+#
+# Copyright (c) 2007 Hal Brodigan (postmodern at users.sourceforge.net)
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+#
+
 require 'rexml/document'
 require 'open-uri'
 require 'uri'
@@ -20,6 +41,9 @@ module Ronin
       # Authors of the repository
       attr_reader :authors
 
+      # License that the repository contents is under
+      attr_reader :license
+
       # Description
       attr_reader :description
 
@@ -37,6 +61,7 @@ module Ronin
 	  repo.each_element('type') { |type| @type = type.get_text.to_s }
 	  repo.each_element('src') { |src| @src = URI.parse(src.get_text.to_s) }
 
+	  repo.each_element('license') { |license| @license = license.get_text.to_s }
 	  repo.each_element('description') { |desc| @description = desc.get_text.to_s }
 
 	  repo.each_element('dependency') do |dep|
