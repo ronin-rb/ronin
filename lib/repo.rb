b6c6fdf910ae028994d78fa44f3752f915707d3a
postmodern <postmodern.mod3@gmail.com>
2007-05-28 01:30:18 +0000

    * Removed Action class in favor of storing the action blocks with in the Context class. * Updated repo.rb.

--- lib/repo.rb
+++ lib/repo.rb
@@ -19,8 +19,8 @@
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #
 
+require 'repo/extensions'
 require 'repo/exceptions'
-require 'repo/action'
 require 'repo/context'
 require 'repo/category'
 require 'repo/objectmetadata'
