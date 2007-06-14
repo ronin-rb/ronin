7a2b7753fea3140180885253b557e8a6fea27267
postmodern <postmodern.mod3@gmail.com>
2007-06-14 03:14:57 +0000

    * Changed class Arch to store all initialized Arch objects in a class variable. * Added the archs method. * Added the all method.

--- lib/arch.rb
+++ lib/arch.rb
@@ -39,20 +39,38 @@ module Ronin
       @arch = arch
       @endian = endian
       @address_length = address_length
+
+      Arch.archs[@arch] = self
+    end
+
+    def Arch.archs
+      @@archs ||= {}
+    end
+
+    def Arch.all(&block)
+      Arch.archs.each_value { |arch| block.call(arch) }
+    end
+
+    X86 = Arch.new("x86",LITTLE_ENDIAN,4)
+    AMD64 = Arch.new("amd64",LITTLE_ENDIAN,8)
+    IA64 = Arch.new("ia64",LITTLE_ENDIAN,8)
+    PPC = Arch.new("ppc",BIG_ENDIAN,4)
+    PPC64 = Arch.new("ppc64",BIG_ENDIAN,8)
+    SPARC = Arch.new("sparc",BIG_ENDIAN,4)
+    SPARC64 = Arch.new("sparc64",BIG_ENDIAN,8)
+    MIPS_LITTLE = Arch.new("mips_little",LITTLE_ENDIAN,4)
+    MIPS_BIG = Arch.new("mips_big",BIG_ENDIAN,4)
+    ARM_LITTLE = Arch.new("arm_little",LITTLE_ENDIAN,4)
+    ARM_BIG = Arch.new("arm_big",BIG_ENDIAN,4)
+
+    protected
+
+    def method_missing(sym,*args)
+      name = sym.id2name
+      return Arch.archs[name] if Arch.archs.has_key?(name)
+
+      raise NoMethodError, name
     end
 
   end
-
-  ARCH_X86 = Arch.new("x86",LITTLE_ENDIAN,4)
-  ARCH_AMD64 = Arch.new("amd64",LITTLE_ENDIAN,8)
-  ARCH_IA64 = Arch.new("ia64",LITTLE_ENDIAN,8)
-  ARCH_PPC = Arch.new("ppc",BIG_ENDIAN,4)
-  ARCH_PPC64 = Arch.new("ppc64",BIG_ENDIAN,8)
-  ARCH_SPARC = Arch.new("sparc",BIG_ENDIAN,4)
-  ARCH_SPARC64 = Arch.new("sparc64",BIG_ENDIAN,8)
-  ARCH_MIPS_LE = Arch.new("mips LE",LITTLE_ENDIAN,4)
-  ARCH_MIPS_BE = Arch.new("mips BE",BIG_ENDIAN,4)
-  ARCH_ARM_LE = Arch.new("arm LE",LITTLE_ENDIAN,4)
-  ARCH_ARM_BE = Arch.new("arm BE",BIG_ENDIAN,4)
-
 end
