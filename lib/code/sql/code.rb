0d7dea899dac7819e687986ab7a3ce2d8fc47873
postmodern <postmodern.mod3@gmail.com>
2007-06-19 04:27:18 +0000

    * First attempt at an SQL(-Injection) DSL.

---
+++ lib/code/sql/code.rb
@@ -0,0 +1,34 @@
+#
+# Ronin - A decentralized repository for the storage and sharing of computer
+# security advisories, exploits and payloads.
+#
+# Copyright (c) 2007 Hal Brodigan (postmodern at users.sourceforge.net)
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+#
+
+require 'code/sql/expression'
+
+module Ronin
+  module Code
+    def Code.sql(opts={:prepend => "", :append => ""},&block)
+      opts[:prepend]+SQL::Expression.compile(&block)+opts[:append]
+    end
+
+    def Code.sql_inject(opts={:prepend => "", :expr => "", :append => ""},&block)
+      opts[:prepend]+SQL::Expression.inject(opts[:expr],&block)+opts[:append]
+    end
+  end
+end
