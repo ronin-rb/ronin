27df31ee204e4c3c5da3d65eafe9d5b128011be3
postmodern <postmodern.mod3@gmail.com>
2007-07-22 00:37:50 +0000

    * Added the Objects DB controller.

--- lib/repo/objectsdb.rb
+++ lib/repo/objectsdb.rb
@@ -4,6 +4,12 @@ module Ronin
   module Repo
     RONIN_OBJECTS_DB_PATH = File.join(RONIN_HOME_PATH,'objects')
 
+    def ronin_objects
+      $ronin_objects_db
+    end
+
+    protected
+
     $ronin_objects_db ||= Og.setup(:destroy => false, :evolve_schema => true, :store => :sqlite, :name => RONIN_OBJECTS_DB_PATH)
 
   end
