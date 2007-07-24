ee2a99fdf91ad6bcbb18de2d1d4384fef5aa8594
postmodern <postmodern.mod3@gmail.com>
2007-07-24 00:26:22 +0000

    * Refined the meta-programming for Parameters. Less code is more elegance.

--- lib/parameters.rb
+++ lib/parameters.rb
@@ -58,96 +58,87 @@ module Ronin
 
   module Parameters
     def self.included(klass)
-      # do not re-parameterize classes
-      return unless klass.include?(self)
-
-      # parameterize the class
-      parameterize(klass)
-
-      # define base-methods for the sub-class parameter methods to recurse to
-      klass.class_eval %{
-        def self.has_class_param?(name)
-	  #{klass}.class_params.has_key?(name.to_sym)
-        end
-
-        def has_class_param?(name)
-          #{klass}.has_class_param?(name)
-        end
-
-	def self.has_param?(name)
-	  has_class_param?(name)
+      klass.metaclass.class_eval do
+	def params
+	  @params ||= {}
 	end
 
-	def instance_params
-	  @instance_params ||= {}
-	end
-
-	def has_instance_param?(name)
-	  instance_params.has_key?(name.to_sym)
-	end
-
-	def self.param(name)
+	def param(name)
 	  name = name.to_sym
 
-	  return #{klass}.class_params[name] if #{klass}.has_class_param?(name)
+	  ancestors.each do |superclass|
+	    if superclass.include?(Parameters)
+	      if superclass.params.has_key?(name)
+		return superclass.params[name]
+	      end
+	    end
+	  end
 
-	  raise ParamNotFound, "parameter '#\{name\}' does not exist", caller
-	end
-
-        def param(name)
-	  name = name.to_sym
-
-	  return instance_params[name] if has_instance_param?(name)
-	  return #{klass}.class_params[name] if #{klass}.has_class_param?(name)
-
-	  raise ParamNotFound, "parameter '#\{name\}' does not exist", caller
+	  return nil
 	end
 
 	def has_param?(name)
 	  name = name.to_sym
 
-	  return true if has_instance_param?(name)
-	  return true if #{klass}.has_class_param?(name)
+	  ancestors.each do |superclass|
+	    if superclass.include?(Parameters)
+	      return true if superclass.params.has_key?(name)
+	    end
+	  end
+
 	  return false
 	end
 
-        def each_param(&block)
-          instance_params.each_value(&block)
-          class_params.each do |key,param|
-	    block.call(param) unless has_instance_param?(key)
-          end
-
-          return self
-        end
-      }
-
-      # have sub-classes automatically inherit the parameters
-      klass.class_eval {
-	def self.inherited(subclass)
-	  Parameters.inherit_parameters(subclass)
+	def each_param(&block)
+	  ancestors.each do |superclass|
+	    if superclass.include?(Parameters)
+	      superclass.params.each { |param| block.call(param) }
+	    end
+	  end
 	end
-      }
+      end
     end
 
     def parameter(name,opts={:value => nil, :desc => ""})
       name = name.to_sym
 
       # add the parameter to the instance parameters
-      instance_params[name] = Param.new(name,opts[:value],opts[:desc])
+      params[name] = Param.new(name,opts[:value],opts[:desc])
 
       # define the reader method for the parameter
       class_def(name) do
-        instance_params[name.to_sym].value
+        params[name.to_sym].value
       end
 
       # define the writer method for the parameter
       class_def("#{name}=") do |value|
-	instance_params[name.to_sym].value = value
+	params[name.to_sym].value = value
       end
+    end
+
+    def class_params
+      self.class.params
+    end
+
+    def params
+      @params ||= {}
+    end
+
+    def param(name)
+      params[name.to_sym]
+    end
+
+    def has_param?(name)
+      params.has_key?(name.to_sym)
     end
 
     def describe_param(name)
       param(name).description
+    end
+
+    def adopt_param(param)
+      value = param.value.clone if param.value
+      params[param.name] = Param.new(param.name,value,param.description)
     end
 
     def import_params(params)
@@ -169,105 +160,35 @@ module Ronin
       name = name.to_sym
 
       # add the parameter to the class params list
-      class_params[name] = Param.new(name,opts[:value],opts[:desc])
+      params[name] = Param.new(name,opts[:value],opts[:desc])
 
       # define the reader class method for the parameter
       meta_def(name) {
-	class_params[name].value
+	params[name].value
       }
 
       # define the writer class method for the parameter
       meta_def("#{name}=") { |value|
-	class_params[name].value = value
+	params[name].value = value
       }
 
       # define the reader instance method for the parameter
       class_def(name) {
-	unless instance_params.has_key?(name)
+	unless params.has_key?(name)
 	  return class_params[name].value.freeze
 	else
-	  return instance_params[name].value
+	  return params[name].value
 	end
       }
 
       # define the writer instance method for the parameter
       class_def("#{name}=") { |value|
-	unless instance_params.has_key?(name)
+	unless params.has_key?(name)
 	  adopt_param(class_params[name])
 	end
 
-	return instance_params[name].value = value
+	return params[name].value = value
       }
-    end
-
-    def self.parameterize(klass)
-      # define the class parameters in the class
-      klass.meta_eval {
-	def class_params
-	  @class_params ||= {}
-	end
-      }
-
-      klass.class_eval {
-	def self.class_params
-	  metaclass.class_params
-	end
-      }
-    end
-
-    def self.inherit_parameters(klass)
-      parameterize(klass)
-
-      klass.class_eval %{
-        def self.has_class_param?(name)
-          name = name.to_sym
-
-          return true if #{klass}.class_params.has_key?(name)
-          return super(name)
-        end
-
-        def self.param(name)
-          name = name.to_sym
-
-	  return #{klass}.class_params[name] if #{klass}.class_params.has_key?(name)
-          return super(name)
-
-	  raise ParamNotFound, "parameter '#\{name\}' does not exist", caller
-        end
-
-        def param(name)
-	  name = name.to_sym
-
-	  return instance_params[name] if has_instance_param?(name)
-	  return #{klass}.class_params[name] if #{klass}.class_params.has_key?(name)
-	  return super(name)
-	end
-
-	def has_param?(name)
-	  name = name.to_sym
-
-	  return true if instance_params.has_key?(name)
-	  return true if #{klass}.class_params.has_key?(name)
-	  return super(name)
-	end
-
-        def each_param(&block)
-          instance_params.each_value(&block)
-          class_params.each do |key,param|
-	    block.call(param) unless instance_params.has_key?(key)
-          end
-
-          super(&block)
-          return self
-        end
-      }
-    end
-
-    private
-
-    def adopt_param(param)
-      value = param.value.clone if param.value
-      instance_params[param.name] = Param.new(param.name,value,param.description)
     end
   end
 end
