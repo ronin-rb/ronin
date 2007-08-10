2a476d531603dcc6841ce3db3f320ff5d49d5819
postmodern <postmodern.mod3@gmail.com>
2007-08-10 01:55:27 +0000

    * Added Config and Environment.

---
+++ lib/repo/config.rb
@@ -0,0 +1,34 @@
+#
+# Ronin - A decentralized repository for the storage and sharing of computer
+# security advisories, exploits and payloads.
+#
+# Copyright (c) 2007 Hal Brodigan (postmodern at users.sourceforge.net)
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+#
+
+require 'fileutils'
+
+module Ronin
+  module Repo
+    module Config
+      # Ronin home directory
+      PATH = File.join(ENV['HOME'],'.ronin')
+
+      # Ronin gem directory
+      GEM_PATH = File.join(PATH,'gems')
+    end
+  end
+end
