40f59412541dfe071520620944d07dbaa180eb57
postmodern <postmodern.mod3@gmail.com>
2007-08-11 23:57:51 +0000

    * Use symbols infavor of strings.

--- lib/formating/extensions/binary/integer.rb
+++ lib/formating/extensions/binary/integer.rb
@@ -26,14 +26,14 @@ class Integer
   def pack(arch,address_length=arch.address_length)
     buffer = ""
 
-    if arch.endian==Ronin::LITTLE_ENDIAN
+    if arch.endian==:big_endian
       mask = 0xff
 
       address_length.times do |i|
         buffer+=((self & mask) >> (i*8)).chr
         mask <<= 8
       end
-    elsif arch.endian==Ronin::BIG_ENDIAN
+    elsif arch.endian==:big_endian
       mask = (0xff << ((address_length-1)*8))
 
       address_length.times do |i|
