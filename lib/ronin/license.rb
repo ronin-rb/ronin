49a046fa5685061faf0fcd4ba547181da30adbd1
postmodern <postmodern.mod3@gmail.com>
2007-09-17 09:52:25 +0000

    * Added the url attribute. * Added some more license constants.

--- lib/ronin/license.rb
+++ lib/ronin/license.rb
@@ -28,11 +28,15 @@ module Ronin
     # Description of license
     attr_reader :description, String
 
+    # URL of the License document
+    attr_reader :url, String
+
     schema_inheritance
 
-    def initialize(name,description=nil)
+    def initialize(name,description=nil,url=nil)
       @name = name.to_s
       @description = description
+      @url = url
 
       License.licenses[@name] = self
     end
@@ -53,8 +57,18 @@ module Ronin
       @name.to_s
     end
 
-    GPL2 = License.new('GPL-2','GNU Public License v2.0')
-    GPL3 = License.new('GPL-3','GNU Public License v3.0')
+    # GNU Public Licenses
+    GPL2 = License.new('GPL-2','GNU Public License v2.0','http://www.gnu.org/licenses/gpl-2.0.txt')
+    GPL3 = License.new('GPL-3','GNU Public License v3.0','http://www.gnu.org/licenses/gpl-3.0.txt')
+    LGPL3 = License.new('LGPL-3','GNU Lesser General Public License v3.0','http://www.gnu.org/licenses/lgpl-3.0.txt')
+
+    # Creative Commons Licenses
+    CC_BY = License.new('CC by','Creative Commons Attribution v3.0 License','http://creativecommons.org/licenses/by/3.0/')
+    CC_BY_SA = License.new('CC by-sa','Creative Commons Attribution-Share Alike v3.0 License','http://creativecommons.org/licenses/by-sa/3.0/')
+    CC_BY_ND = License.new('CC by-nd','Creative Commons Attribution-No Derivative Works v3.0 License','http://creativecommons.org/licenses/by-nd/3.0/')
+    CC_BY_NC = License.new('CC by-nc','Creative Commons Attribution-Noncommercial v3.0 License','http://creativecommons.org/licenses/by-nc/3.0/')
+    CC_BY_NC_SA = License.new('CC by-nc-sa','Creative Commons Attribution-Noncommercial-Share Alike v3.0 License','http://creativecommons.org/licenses/by-nc-sa/3.0/')
+    CC_BY_NC_ND = License.new('CC by-nc-nd','Creative Commons Attribution-Noncommercial-No Derivative Works v3.0 License','http://creativecommons.org/licenses/by-nc-nd/3.0/')
 
   end
 end
