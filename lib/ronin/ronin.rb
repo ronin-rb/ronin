ba51b1d109d37762a51f91001b6dfd25ea24e95d
postmodern <postmodern.mod3@gmail.com>
2008-01-01 06:05:20 +0000

    * Forgot to update this path.

--- lib/ronin/ronin.rb
+++ lib/ronin/ronin.rb
@@ -19,7 +19,7 @@
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #
 
-require 'ronin/objectcache'
+require 'ronin/object_cache'
 require 'ronin/arch'
 require 'ronin/platform'
 require 'ronin/license'
