b33e3fcb0a98787c6e424a78e8dc84b14fc63301
postmodern <postmodern.mod3@gmail.com>
2008-01-05 12:35:08 +0000

    * Added documentation. * Pass a block to Product#initialize. * Changed attr_reader to attr_accessor.

--- lib/ronin/product.rb
+++ lib/ronin/product.rb
@@ -26,26 +26,41 @@ module Ronin
   class Product
 
     # Name
-    attr_reader :name, String
+    attr_accessor :name, String
 
     # Version
-    attr_reader :version, String
+    attr_accessor :version, String
 
     # Venders
-    attr_reader :vendor, String
+    attr_accessor :vendor, String
 
-    def initialize(name,version,vendor=name)
+    #
+    # Creates a new Product with the specified _name_ and _version_,
+    # and the given _vendor_. The _vendor_ defaults to the _name_.
+    # If _block_ is given, it will be passed the newly created Product
+    # object.
+    #
+    def initialize(name,version,vendor=name,&block)
       @name = name.to_s
       @version = version.to_s
       @vendor = vendor.to_s
+
+      block.call(self) if block
     end
 
+    #
+    # Returns true if the product has the same name, version and vendor
+    # of the _other_ product, returns false otherwise.
+    #
     def ==(other)
       return false unless @name==other.name
       return false unless @version==other.version
       return @vendor==other.vendor
     end
 
+    #
+    # Returns the String form of the product.
+    #
     def to_s
       unless @vendor==@name
         return "#{@vendor} #{@name} #{@version}"
@@ -54,7 +69,7 @@ module Ronin
       end
     end
 
-    def self.parse_xml(doc,xpath='/ronin/product')
+    def self.from_xml(doc,xpath='/ronin/product')
       products = []
 
       doc.element.each(xpath) do |element|
