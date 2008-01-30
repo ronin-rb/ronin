81947e4e32372a6fd6db175c572ed3d1095f152e
postmodern <postmodern.mod3@gmail.com>
2008-01-30 22:25:48 +0000

    * Adding config.rb.

---
+++ lib/ronin/repo/config.rb
@@ -0,0 +1,34 @@
+#
+#--
+# Ronin - A ruby development platform designed for information security
+# and data exploration tasks.
+#
+# Copyright (c) 2006-2008 Hal Brodigan (postmodern.mod3 at gmail.com)
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+#++
+#
+
+require 'ronin/config'
+
+module Ronin
+  module Config
+    # Path to repositories directory
+    REPOS_DIR = File.join(Config::PATH,'repos')
+
+    # Path to cache file
+    REPOS_CACHE_PATH = File.join(Config::PATH,'cache.yml')
+  end
+end
