275c5b0161f417d6f8a9a662d4bb663e7343009c
postmodern <postmodern.mod3@gmail.com>
2008-02-03 07:28:09 +0000

    * Do not pass methods down the Og Store, that could be dangerous for the db.

--- lib/ronin/object_cache.rb
+++ lib/ronin/object_cache.rb
@@ -1,8 +1,9 @@
 #
-# Ronin - A ruby development environment designed for information security
+#--
+# Ronin - A ruby development platform designed for information security
 # and data exploration tasks.
 #
-# Copyright (c) 2006-2007 Hal Brodigan (postmodern.mod3 at gmail.com)
+# Copyright (c) 2006-2008 Hal Brodigan (postmodern.mod3 at gmail.com)
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -17,6 +18,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+#++
 #
 
 require 'ronin/config'
@@ -26,7 +28,8 @@ require 'og'
 module Ronin
   class ObjectCache
 
-    STORE_PATH = File.join(Config::PATH,'object_cache')
+    # Default path for the object cache.
+    PATH = File.join(Config::PATH,'object_cache')
 
     # Path of the object cache
     attr_reader :path
@@ -34,21 +37,20 @@ module Ronin
     # Object cache store
     attr_reader :store
 
-    def initialize(path=STORE_PATH)
+    #
+    # Creates a new ObjectCache object with the given _path_. The _path_
+    # defaults to PATH. If _block_ is given, it will be passed the newly
+    # created ObjectCache object.
+    #
+    #   ObjectCache.new # => ObjectCache
+    #
+    #   ObjectCache.new('/path/to/cache') # => ObjectCache
+    #
+    def initialize(path=PATH,&block)
       @path = path
       @store = Og.setup(:destroy => false, :evolve_schema => true, :store => :sqlite, :name => @path)
-    end
 
-    def sql(sql)
-      @store.get_store.exec_statement(sql)
-    end
-
-    protected
-
-    def method_missing(sym,*args,&block)
-      if @store.respond_to?(sym)
-        return @store.send(sym,*args,&block)
-      end
+      block.call(self) if block
     end
 
   end
