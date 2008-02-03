1e2c687685854659a4e5f690f20c50069397828c
postmodern <postmodern.mod3@gmail.com>
2008-02-03 06:28:23 +0000

    * Updated ObjectContext.   * Don't include Context, contextify does this automatically.   * Use from_xml of objects if defined.

--- lib/ronin/repo/object_context.rb
+++ lib/ronin/repo/object_context.rb
@@ -1,8 +1,9 @@
 #
-# Ronin - A ruby development environment designed for information security
+#--
+# Ronin - A ruby development platform designed for information security
 # and data exploration tasks.
 #
-# Copyright (c) 2006-2007 Hal Brodigan (postmodern.mod3 at gmail.com)
+# Copyright (c) 2006-2008 Hal Brodigan (postmodern.mod3 at gmail.com)
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -17,6 +18,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+#++
 #
 
 require 'ronin/repo/context'
@@ -30,16 +32,27 @@ require 'rexml/document'
 module Ronin
   module Repo
     module ObjectContext
-      include Context
-
+      #
+      # Returns the Hash of all defined object-contexts.
+      #
       def ObjectContext.object_contexts
         @@object_contexts ||= {}
       end
 
-      def ObjectContext.is_object_context?(id)
-        ObjectContext.object_contexts.has_key?(id.to_sym)
+      #
+      # Returns +true+ if there is an object-context defined with the
+      # specified _name_, returns +false+ otherwise.
+      #
+      def ObjectContext.is_object_context?(name)
+        ObjectContext.object_contexts.has_key?(name.to_sym)
       end
 
+      #
+      # Loads all object-contexts from the specified _path_, returning an 
+      # Array of the loaded objects.
+      #
+      #   ObjectContext.load_objects('/path/to/obj.rb') # => [...]
+      #
       def ObjectContext.load_objects(path)
         path = File.expand_path(path)
         objects = []
@@ -59,14 +72,23 @@ module Ronin
         return objects
       end
 
-      def ObjectContext.load_object(type,path,*args)
-        type = type.to_sym
+      #
+      # Load the object-context of the specified _name_, from the specified
+      # _path_ with the given _args_. If no object-contexts were defined
+      # with the specified _name_, an ObjectNotFound exception will be
+      # raised.
+      #
+      #   ObjectContext.load_object('payload','/path/to/custom_payload.rb')
+      #     # => Payload
+      #
+      def ObjectContext.load_object(name,path,*args)
+        name = name.to_sym
 
-        unless ObjectContext.is_object_context?(type)
-          raise(ObjectNotFound,"unknown object context '#{type}'",caller)
+        unless ObjectContext.is_object_context?(name)
+          raise(ObjectNotFound,"unknown object context '#{name}'",caller)
         end
 
-        return ObjectContext.object_contexts[type].create_object(path,*args)
+        return ObjectContext.object_contexts[name].create_object(path,*args)
       end
 
       def ObjectContext.namify(base)
@@ -75,27 +97,27 @@ module Ronin
 
       protected
 
-      def Object.object_contextify(id=ObjectContext.namify(self))
-        class_eval do
-          # contextify the class
-          contextify(id)
+      def Object.object_contextify(name=ObjectContext.namify(self))
+        # contextify the class
+        contextify(name)
 
-          # make the class taggable
-          include Taggable
+        include ObjectContext
 
-          # the path of the object context file
-          attr_accessor :object_path, String
+        # make the class taggable
+        include Taggable
 
-          before %{
-            path = res[res.fields.index('object_path')]
-            if path
-              load_object(path)
+        # the path of the object context file
+        attr_accessor :object_path, String
 
-              @oid = res[res.fields.index('oid')]
-              return
-            end
-          }, :on => :og_read
-        end
+        before %{
+          path = res[res.fields.index('object_path')]
+          if path
+            load_object(path)
+
+            @oid = res[res.fields.index('oid')]
+            return
+          end
+        }, :on => :og_read
 
         if Ronin.object_cache_loaded?
           # manage classes after the object cache has been setup
@@ -122,9 +144,9 @@ module Ronin
 
         # define Repo-level object loader method
         Ronin.module_eval %{
-          def ronin_load_#{id}(path,&block)
-            if (File.extname(path)=='.xml' && #{self}.respond_to?(:parse_xml))
-              return #{self}.parse_xml(REXML::Document.new(path))
+          def ronin_load_#{name}(path,&block)
+            if (File.extname(path)=='.xml' && #{self}.respond_to?(:from_xml))
+              return #{self}.from_xml(REXML::Document.new(path))
             else
               return #{self}.create_object(path,&block)
             end
@@ -132,7 +154,7 @@ module Ronin
         }
 
         # add the class to the global list of object contexts
-        ObjectContext.object_contexts[id] = self
+        ObjectContext.object_contexts[name] = self
       end
     end
   end
