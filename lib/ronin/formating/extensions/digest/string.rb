15d5cfcb8a1e42e0e7450f3233737f58cdd75459
postmodern <postmodern.mod3@gmail.com>
2008-05-18 09:47:17 +0000

    * Added Digest formatting extensions to String.

---
+++ lib/ronin/formating/extensions/digest/string.rb
@@ -0,0 +1,50 @@
+#
+#--
+# Ronin - A ruby development platform designed for information security
+# and data exploration tasks.
+#
+# Copyright (c) 2006-2008 Hal Brodigan (postmodern.mod3 at gmail.com)
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+#++
+#
+
+require 'digest/md5'
+require 'digest/sha1'
+require 'digest/sha2'
+
+class String
+
+  def md5
+    Digest::MD5.hexdigest(self)
+  end
+
+  def sha1
+    Digest::SHA1.hexdigest(self)
+  end
+
+  def sha2
+    Digest::SHA2.hexdigest(self)
+  end
+
+  def sha256
+    Digest::SHA256.hexdigest(self)
+  end
+
+  def sha512
+    Digest::SHA512.hexdigest(self)
+  end
+
+end
