a9ecdeae29f372feeacfa5555e8e62929bdc3b52
postmodern <postmodern.mod3@gmail.com>
2008-10-29 14:27:20 -0700

    Wording change.

--- spec/ronin_spec.rb
+++ spec/ronin_spec.rb
@@ -3,7 +3,7 @@ require 'ronin/version'
 require 'spec_helper'
 
 describe Ronin do
-  it "should have a version constant" do
+  it "should have a version" do
     @version = Ronin.const_get('VERSION')
     @version.should_not be_nil
     @version.should_not be_empty
