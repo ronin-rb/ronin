4ee7adf05841732555d776559652a42af977902f
postmodern <postmodern.mod3@gmail.com>
2008-12-03 21:34:50 -0800

    Added a spec for ObjectContext.

---
+++ spec/object_context/object_context_spec.rb
@@ -0,0 +1,29 @@
+require 'ronin/object_context'
+
+require 'spec_helper'
+
+describe ObjectContext do
+  before(:all) do
+    class TestObject
+
+      include ObjectContext
+
+      object_contextify :test
+
+      property :mesg, String
+
+      parameter :x
+
+      parameter :y
+
+    end
+  end
+
+  it "should create an object with a Hash of attributes and params" do
+    test = TestObject.new(:mesg => 'hello', :x => 3, :y => 9)
+
+    test.mesg.should == 'hello'
+    test.x.should == 3
+    test.y.should == 9
+  end
+end
