2b183f89f52e1f4c0a89c52b55033c381956cfff
postmodern <postmodern.mod3@gmail.com>
2008-12-11 02:32:31 -0800

    Added a ParamParser for parsing params from the command-line.

---
+++ lib/ronin/ui/command_line/param_parser.rb
@@ -0,0 +1,71 @@
+#
+#--
+# Ronin - A Ruby platform designed for information security and data
+# exploration tasks.
+#
+# Copyright (c) 2006-2008 Hal Brodigan (postmodern.mod3 at gmail.com)
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+#++
+#
+
+require 'uri'
+
+module Ronin
+  module UI
+    module CommandLine
+      module ParamParser
+        # Hash of format patterns and their parsers
+        FORMATS = {
+          /^[0-9]+$/ => lambda { |value| value.to_i },
+          /^0x[0-9a-fA-F]+$/ => lambda { |value| value.hex },
+          /^[a-zA-Z][a-zA-Z0-9]*:\/\// => lambda { |value| URI(value) },
+          'true' => lambda { |value| true },
+          'false' => lambda { |value| false }
+        }
+
+        # The params Hash
+        attr_reader :params
+
+        #
+        # Creates an empty +params+ Hash.
+        #
+        def initialize
+          @params = {}
+        end
+
+        #
+        # Parses the specified _name_and_value_ string of the form
+        # "name=value" and extracts both the _name_ and the _value_, saving
+        # both the _name_ and _value_ within the +params+ Hash. If the
+        # extracted _value_ matches one of the patterns within +FORMATS+,
+        # then the associated parser will first parse the _value_.
+        #
+        def parse_param(name_and_value)
+          name, value = name_and_value.split('=',2)
+
+          FORMATS.each do |pattern,parser|
+            if value.match(pattern)
+              value = parser.call(value)
+              break
+            end
+          end
+
+          @params[name.to_sym] = value
+        end
+      end
+    end
+  end
+end
---
+++ spec/ui/command_line/param_parser_spec.rb
@@ -0,0 +1,51 @@
+require 'ronin/ui/command_line/param_parser'
+
+require 'spec_helper'
+
+describe UI::CommandLine::ParamParser do
+  before(:all) do
+    class ExampleCommand
+
+      include UI::CommandLine::ParamParser
+
+    end
+
+    @command = ExampleCommand.new
+  end
+
+  it "should provide a params Hash" do
+    @command.params.should_not be_nil
+  end
+
+  it "should parser params of the form 'name=value'" do
+    @command.parse_param('var=test')
+    @command.params[:var].should == 'test'
+  end
+
+  it "should parse params which have numeric values" do
+    @command.parse_param('var2=100')
+    @command.params[:var2].should == 100
+  end
+
+  it "should parse params with hexadecimal values" do
+    @command.parse_param('var3=0x2a')
+    @command.params[:var3].should == 0x2a
+  end
+
+  it "should parse params with URI values" do
+    url = 'http://example.com/'
+
+    @command.parse_param("var4=#{url}")
+    @command.params[:var4].should == URI(url)
+  end
+
+  it "should parse params with true boolean values" do
+    @command.parse_param('var5=true')
+    @command.params[:var5].should == true
+  end
+
+  it "should parse params with false boolean values" do
+    @command.parse_param('var6=false')
+    @command.params[:var6].should == false
+  end
+end
