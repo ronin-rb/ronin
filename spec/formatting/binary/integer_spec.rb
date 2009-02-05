3a1d3882f988bdd28144b80218aaa626c377fa1d
postmodern <postmodern.mod3@gmail.com>
2009-02-05 03:38:02 -0800

    Style.

--- spec/formatting/binary/integer_spec.rb
+++ spec/formatting/binary/integer_spec.rb
@@ -3,53 +3,55 @@ require 'ronin/formatting/binary'
 require 'spec_helper'
 
 describe Integer do
-  before(:all) do
-    @integer = 0x1337
-
-    @i386_packed_int = "7\023\000\000"
-    @i386_packed_short = "7\023"
-    @i386_packed_long = "7\023\000\000"
-    @i386_packed_quad = "7\023\000\000\000\000\000\000"
-
-    @ppc_packed_int = "\000\000\0237"
-    @ppc_packed_short = "\0237"
-    @ppc_packed_long = "\000\000\0237"
-    @ppc_packed_quad = "\000\000\000\000\000\000\0237"
-  end
-
   it "should provide Integer#pack" do
     @integer.respond_to?('pack').should == true
   end
 
-  it "Integer#pack should pack itself for a little-endian architecture" do
-    @integer.pack(Arch.i386).should == @i386_packed_int
-  end
+  describe "pack" do
+    before(:all) do
+      @integer = 0x1337
 
-  it "Integer#pack should pack itself as a short for a little-endian architecture" do
-    @integer.pack(Arch.i386,2).should == @i386_packed_short
-  end
+      @i386_packed_int = "7\023\000\000"
+      @i386_packed_short = "7\023"
+      @i386_packed_long = "7\023\000\000"
+      @i386_packed_quad = "7\023\000\000\000\000\000\000"
 
-  it "Integer#pack should pack itself as a long for a little-endian architecture" do
-    @integer.pack(Arch.i386,4).should == @i386_packed_long
-  end
+      @ppc_packed_int = "\000\000\0237"
+      @ppc_packed_short = "\0237"
+      @ppc_packed_long = "\000\000\0237"
+      @ppc_packed_quad = "\000\000\000\000\000\000\0237"
+    end
 
-  it "Integer#pack should pack itself as a quad for a little-endian architecture" do
-    @integer.pack(Arch.i386,8).should == @i386_packed_quad
-  end
+    it "should pack itself for a little-endian architecture" do
+      @integer.pack(Arch.i386).should == @i386_packed_int
+    end
 
-  it "Integer#pack should pack itself for a big-endian architecture" do
-    @integer.pack(Arch.ppc).should == @ppc_packed_int
-  end
+    it "should pack itself as a short for a little-endian architecture" do
+      @integer.pack(Arch.i386,2).should == @i386_packed_short
+    end
 
-  it "Integer#pack should pack itself as a short for a big-endian architecture" do
-    @integer.pack(Arch.ppc,2).should == @ppc_packed_short
-  end
+    it "Integer#pack should pack itself as a long for a little-endian architecture" do
+      @integer.pack(Arch.i386,4).should == @i386_packed_long
+    end
 
-  it "Integer#pack should pack itself as a long for a big-endian architecture" do
-    @integer.pack(Arch.ppc,4).should == @ppc_packed_long
-  end
+    it "should pack itself as a quad for a little-endian architecture" do
+      @integer.pack(Arch.i386,8).should == @i386_packed_quad
+    end
 
-  it "Integer#pack should pack itself as a quad for a big-endian architecture" do
-    @integer.pack(Arch.ppc,8).should == @ppc_packed_quad
+    it "should pack itself for a big-endian architecture" do
+      @integer.pack(Arch.ppc).should == @ppc_packed_int
+    end
+
+    it "should pack itself as a short for a big-endian architecture" do
+      @integer.pack(Arch.ppc,2).should == @ppc_packed_short
+    end
+
+    it "should pack itself as a long for a big-endian architecture" do
+      @integer.pack(Arch.ppc,4).should == @ppc_packed_long
+    end
+
+    it "should pack itself as a quad for a big-endian architecture" do
+      @integer.pack(Arch.ppc,8).should == @ppc_packed_quad
+    end
   end
 end
