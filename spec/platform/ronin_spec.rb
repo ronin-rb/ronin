28f699b5ad5904829e4d2ddc82cd625068c374cb
postmodern <postmodern.mod3@gmail.com>
2009-02-06 15:23:01 -0800

    Added a spec for Ronin.method_missing.

---
+++ spec/platform/ronin_spec.rb
@@ -0,0 +1,22 @@
+require 'ronin/platform/ronin'
+
+require 'platform/helpers/overlays'
+require 'spec_helper'
+
+describe Ronin do
+  before(:all) do
+    Platform.load_overlays(OVERLAY_CACHE)
+  end
+
+  it "should provide transparent access to extensions" do
+    ext = ::Ronin.hello
+
+    ext.should_not be_nil
+    ext.name.should == 'hello'
+    ext.greatings.should == 'hello'
+  end
+
+  it "should raise NoMethodError when accessing missing extensions" do
+    lambda { Ronin.nothing }.should raise_error(NoMethodError)
+  end
+end
