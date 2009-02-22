226fc4caae727b22a22ef9e4f325375ca2fa784b
postmodern <postmodern.mod3@gmail.com>
2009-02-21 18:12:40 -0800

    Update ParamParser spec.

--- spec/ui/command_line/param_parser_spec.rb
+++ spec/ui/command_line/param_parser_spec.rb
@@ -5,49 +5,45 @@ require 'ui/command_line/helpers/example_command'
 
 describe UI::CommandLine::ParamParser do
   before(:all) do
-    @command = ExampleCommand.new
-  end
-
-  it "should provide a params Hash" do
-    @command.params.should_not be_nil
+    @command = ExampleCommand.new('example')
   end
 
   it "should parse params of the form 'name'" do
-    @command.parse_param('var')
+    @command.param_parser('var')
 
     @command.params.has_key?(:var).should == true
     @command.params[:var].should be_nil
   end
 
   it "should parse params of the form 'name=value'" do
-    @command.parse_param('var1=test')
+    @command.param_parser('var1=test')
     @command.params[:var1].should == 'test'
   end
 
   it "should parse params which have numeric values" do
-    @command.parse_param('var2=100')
+    @command.param_parser('var2=100')
     @command.params[:var2].should == 100
   end
 
   it "should parse params with hexadecimal values" do
-    @command.parse_param('var3=0x2a')
+    @command.param_parser('var3=0x2a')
     @command.params[:var3].should == 0x2a
   end
 
   it "should parse params with URI values" do
     url = 'http://example.com/'
 
-    @command.parse_param("var4=#{url}")
+    @command.param_parser("var4=#{url}")
     @command.params[:var4].should == URI(url)
   end
 
   it "should parse params with true boolean values" do
-    @command.parse_param('var5=true')
+    @command.param_parser('var5=true')
     @command.params[:var5].should == true
   end
 
   it "should parse params with false boolean values" do
-    @command.parse_param('var6=false')
+    @command.param_parser('var6=false')
     @command.params[:var6].should == false
   end
 end
