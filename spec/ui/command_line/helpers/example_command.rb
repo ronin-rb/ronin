4d43b4cb78454685876eb5242d942e5486d496e4
postmodern <postmodern.mod3@gmail.com>
2009-02-21 18:12:20 -0800

    Make the ExampleCommand inherit CommandLine::Command.

--- spec/ui/command_line/helpers/example_command.rb
+++ spec/ui/command_line/helpers/example_command.rb
@@ -1,5 +1,21 @@
-class ExampleCommand
+require 'ronin/ui/command_line/command'
+
+class ExampleCommand < UI::CommandLine::Command
 
   include UI::CommandLine::ParamParser
+
+  attr_reader :params
+
+  def initialize(name)
+    @params = {}
+
+    super(name)
+  end
+
+  def param_parser(name_and_value)
+    @params.merge!(parse_param(name_and_value))
+
+    return @params
+  end
 
 end
