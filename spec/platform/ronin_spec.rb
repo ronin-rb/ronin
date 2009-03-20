716acdbe9d2b830cacfc68f7e4d50b1560f22be1
postmodern <postmodern.mod3@gmail.com>
2009-03-20 02:43:05 -0700

    Added specs for Ronin.const_missing which provides transparent access to Extensions.

--- spec/platform/ronin_spec.rb
+++ spec/platform/ronin_spec.rb
@@ -8,7 +8,19 @@ describe Ronin do
     Platform.load_overlays(overlay_cache_path)
   end
 
-  it "should provide transparent access to extensions" do
+  it "should provide transparent access to extensions via methods" do
+    ext = Ronin::Hello
+
+    ext.should_not be_nil
+    ext.name.should == 'hello'
+    ext.greatings.should == 'hello'
+  end
+
+  it "should raise NameError when accessing missing extensions" do
+    lambda { Ronin::Nothing }.should raise_error(NameError)
+  end
+
+  it "should provide transparent access to extensions via methods" do
     ext = Ronin.hello
 
     ext.should_not be_nil
