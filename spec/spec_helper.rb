69de1adc4aad2665958ca58ca9b0124c88b56395
postmodern <postmodern.mod3@gmail.com>
2009-04-02 01:19:11 -0700

    Don't setup the Database by default.
    * Have helpers/database.rb handle setting up the Database.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,10 +3,5 @@ gem 'rspec', '>=1.1.3'
 require 'spec'
 
 require 'ronin/version'
-require 'ronin/database'
 
 include Ronin
-
-require 'helpers'
-
-Database.setup!(:adapter => 'sqlite3', :database => ':memory:')
