2f82d139a8626b80859dd3bc602312419b79587c
postmodern <postmodern.mod3@gmail.com>
2009-04-04 16:02:38 -0700

    Use Database.setup like before.

--- spec/helpers/database.rb
+++ spec/helpers/database.rb
@@ -2,4 +2,4 @@ require 'spec_helper'
 
 require 'ronin/database'
 
-Database.setup!({ :adapter => 'sqlite3', :database => ':memory:' })
+Database.setup({ :adapter => 'sqlite3', :database => ':memory:' })
