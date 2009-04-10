e6af582b97256578197bac8552e69ec4640af28b
postmodern <postmodern.mod3@gmail.com>
2009-04-10 16:36:09 -0700

    Added some specs for IPAddr.each.

--- spec/extensions/ip_addr_spec.rb
+++ spec/extensions/ip_addr_spec.rb
@@ -1,21 +1,42 @@
 require 'ronin/extensions/ip_addr'
 
 describe IPAddr do
-  before(:all) do
-    @fixed_addr = IPAddr.new('10.1.1.2')
-    @class_c = IPAddr.new('10.1.1.2/24')
+  describe "CIDR addresses" do
+    before(:all) do
+      @fixed_addr = IPAddr.new('10.1.1.2')
+      @class_c = IPAddr.new('10.1.1.2/24')
+    end
+
+    it "should only iterate over one IP address for an address" do
+      addresses = @fixed_addr.map { |ip| IPAddr.new(ip) }
+
+      addresses.length.should == 1
+      @fixed_addr.include?(addresses.first)
+    end
+
+    it "should iterate over all IP addresses contained within the IP range" do
+      @class_c.each do |ip|
+        @class_c.include?(IPAddr.new(ip)).should == true
+      end
+    end
   end
 
-  it "should only iterate over one IP address for an address" do
-    addresses = @fixed_addr.map { |ip| IPAddr.new(ip) }
+  describe "NMap style addresses" do
+    before(:all) do
+      @ipv4_range = '10.1.1-5.1'
+      @ipv6_range = '::ff::02-0a::c3'
+    end
 
-    addresses.length.should == 1
-    @fixed_addr.include?(addresses.first)
-  end
+    it "should iterate over all IP addresses in an IPv4 range" do
+      IPAddr.each(@ipv4_range) do |ip|
+        ip =~ /^10\.1\.[1-5]\.1$/
+      end
+    end
 
-  it "should iterate over all IP addresses contained within the IP range" do
-    @class_c.each do |ip|
-      @class_c.include?(IPAddr.new(ip)).should == true
+    it "should iterate over all IP addresses in an IPv6 range" do
+      IPAddr.each(@ipv6_range) do |ip|
+        ip =~ /^::ff::0[2-9a]::c3$/
+      end
     end
   end
 end
