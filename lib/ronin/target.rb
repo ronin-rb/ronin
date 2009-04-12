b920a6d62823d4660d086dbe140c52dead6ed8c0
postmodern <postmodern.mod3@gmail.com>
2009-04-12 04:05:16 -0700

    Forgot to require 'ronin/formatting/binary'.

--- lib/ronin/target.rb
+++ lib/ronin/target.rb
@@ -19,6 +19,7 @@
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #
 
+require 'ronin/formatting/binary'
 require 'ronin/model'
 require 'ronin/os'
 require 'ronin/arch'
