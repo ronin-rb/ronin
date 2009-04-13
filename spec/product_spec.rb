cd88f3ea0b60f6a441b83f50fc5fe581b8a586bd
postmodern <postmodern.mod3@gmail.com>
2009-04-12 22:13:40 -0700

    Added a spec for the defaulting of vendor to the name of the product.

--- spec/product_spec.rb
+++ spec/product_spec.rb
@@ -13,4 +13,11 @@ describe Product do
     @product.version = '0.1.0'
     @product.should be_valid
   end
+
+  it "should default the vendor to the name of the product" do
+    @product = Product.new(:name => 'Adobe', :version => '0.1.0')
+    @product.vendor.should == @product.name
+
+    @product.should be_valid
+  end
 end
