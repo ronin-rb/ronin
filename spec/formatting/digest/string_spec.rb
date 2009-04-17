9a9b869eea2c0ceb81c5ef95bafd5209a05b625d
postmodern <postmodern.mod3@gmail.com>
2009-04-16 19:41:59 -0700

    Restructured the specs for digest and http formatting extensions.

--- spec/formatting/digest/string_spec.rb
+++ spec/formatting/digest/string_spec.rb
@@ -4,51 +4,81 @@ require 'spec_helper'
 
 describe String do
   before(:all) do
-    @digest_plain_text = "test"
-    @digest_md5 = "098f6bcd4621d373cade4e832627b4f6"
-    @digest_sha1 = "a94a8fe5ccb19ba61c4c0873d391e987982fbbd3"
-    @digest_sha2 = "9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08"
-    @digest_sha256 = "9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08"
-    @digest_sha512 = "ee26b0dd4af7e749aa1a8ee3c10ae9923f618980772e473f8819a5d4940e0db27ac185f8a0e1d5f84f88bc887fd67b143732c304cc5fa9ad8e6f57f50028a8ff"
+    @string = "test"
   end
 
   it "should provide String#md5" do
-    @digest_plain_text.respond_to?('md5').should == true
-  end
-
-  it "String#md5 should return the MD5 digest of itself" do
-    @digest_plain_text.md5.should == @digest_md5
+    @string.respond_to?('md5').should == true
   end
 
   it "should provide String#sha1" do
-    @digest_plain_text.respond_to?('sha1').should == true
-  end
-
-  it "String#sha1 should return the SHA1 digest of itself" do
-    @digest_plain_text.sha1.should == @digest_sha1
+    @string.respond_to?('sha1').should == true
   end
 
   it "should provide String#sha2" do
-    @digest_plain_text.respond_to?('sha2').should == true
-  end
-
-  it "String#sha2 should return the SHA2 digest of itself" do
-    @digest_plain_text.sha2.should == @digest_sha2
+    @string.respond_to?('sha2').should == true
   end
 
   it "should provide String#sha256" do
-    @digest_plain_text.respond_to?('sha256').should == true
-  end
-
-  it "String#sha256 should return the SHA256 digest of itself" do
-    @digest_plain_text.sha256.should == @digest_sha256
+    @string.respond_to?('sha256').should == true
   end
 
   it "should provide String#sha512" do
-    @digest_plain_text.respond_to?('sha512').should == true
+    @string.respond_to?('sha512').should == true
   end
 
-  it "String#sha512 should return the SHA512 digest of itself" do
-    @digest_plain_text.sha512.should == @digest_sha512
+  describe "md5" do
+    before(:all) do
+      @digest_plain_text = "test"
+      @digest_md5 = "098f6bcd4621d373cade4e832627b4f6"
+    end
+
+    it "should return the MD5 digest of itself" do
+      @digest_plain_text.md5.should == @digest_md5
+    end
+  end
+
+  describe "sha1" do
+    before(:all) do
+      @digest_plain_text = "test"
+      @digest_sha1 = "a94a8fe5ccb19ba61c4c0873d391e987982fbbd3"
+    end
+
+    it "should return the SHA1 digest of itself" do
+      @digest_plain_text.sha1.should == @digest_sha1
+    end
+  end
+
+  describe "sha2" do
+    before(:all) do
+      @digest_plain_text = "test"
+      @digest_sha2 = "9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08"
+    end
+
+    it "should return the SHA2 digest of itself" do
+      @digest_plain_text.sha2.should == @digest_sha2
+    end
+  end
+
+  describe "sha256" do
+    before(:all) do
+      @digest_plain_text = "test"
+      @digest_sha256 = "9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08"
+    end
+
+    it "should return the SHA256 digest of itself" do
+      @digest_plain_text.sha256.should == @digest_sha256
+    end
+  end
+
+  describe "sha512" do
+    before(:all) do
+      @digest_plain_text = "test"
+      @digest_sha512 = "ee26b0dd4af7e749aa1a8ee3c10ae9923f618980772e473f8819a5d4940e0db27ac185f8a0e1d5f84f88bc887fd67b143732c304cc5fa9ad8e6f57f50028a8ff"
+    end
+
+    it "should return the SHA512 digest of itself" do
+      @digest_plain_text.sha512.should == @digest_sha512
+    end
   end
 end
--- spec/formatting/http/string_spec.rb
+++ spec/formatting/http/string_spec.rb
@@ -4,50 +4,81 @@ require 'spec_helper'
 
 describe String do
   before(:all) do
-    @uri_unencoded = "mod % 3"
-    @uri_encoded = "mod%20%25%203"
-    @uri_http_encoded = "%6d%6f%64%20%25%20%33"
-    @uri_unescaped = "x + y"
-    @uri_escaped = "x+%2B+y"
+    @string = "mod % 3"
   end
 
   it "should provide String#uri_encode" do
-    @uri_unencoded.respond_to?('uri_encode').should == true
-  end
-
-  it "String#uri_encode should URI encode itself" do
-    @uri_unencoded.uri_encode.should == @uri_encoded
+    @string.respond_to?('uri_encode').should == true
   end
 
   it "should provide String#uri_decode" do
-    @uri_encoded.respond_to?('uri_decode').should == true
-  end
-
-  it "String#uri_decode should URI decode itself" do
-    @uri_encoded.uri_decode.should == @uri_unencoded
+    @string.respond_to?('uri_decode').should == true
   end
 
   it "should provide String#uri_escape" do
-    @uri_unescaped.respond_to?('uri_escape').should == true
-  end
-
-  it "String#uri_escape should URI escape itself" do
-    @uri_unescaped.uri_escape.should == @uri_escaped
+    @string.respond_to?('uri_escape').should == true
   end
 
   it "should provide String#uri_unescape" do
-    @uri_escaped.respond_to?('uri_unescape').should == true
-  end
-
-  it "String#uri_unescape should URI unescape itself" do
-    @uri_escaped.uri_unescape.should == @uri_unescaped
+    @string.respond_to?('uri_unescape').should == true
   end
 
   it "should provide String#format_http" do
-    @uri_unencoded.respond_to?('format_http').should == true
+    @string.respond_to?('format_http').should == true
   end
 
-  it "String#format_http should format each byte of the String" do
-    @uri_unencoded.format_http.should == @uri_http_encoded
+  describe "uri_encode" do
+    before(:all) do
+      @uri_unencoded = "mod % 3"
+      @uri_encoded = "mod%20%25%203"
+    end
+
+    it "should URI encode itself" do
+      @uri_unencoded.uri_encode.should == @uri_encoded
+    end
+  end
+
+  describe "uri_decode" do
+    before(:all) do
+      @uri_unencoded = "mod % 3"
+      @uri_encoded = "mod%20%25%203"
+    end
+
+    it "should URI decode itself" do
+      @uri_encoded.uri_decode.should == @uri_unencoded
+    end
+  end
+
+  describe "uri_escape" do
+    before(:all) do
+      @uri_unescaped = "x + y"
+      @uri_escaped = "x+%2B+y"
+    end
+
+    it "should URI escape itself" do
+      @uri_unescaped.uri_escape.should == @uri_escaped
+    end
+  end
+
+  describe "uri_unescape" do
+    before(:all) do
+      @uri_unescaped = "x + y"
+      @uri_escaped = "x+%2B+y"
+    end
+
+    it "should URI unescape itself" do
+      @uri_escaped.uri_unescape.should == @uri_unescaped
+    end
+  end
+
+  describe "format_http" do
+    before(:all) do
+      @uri_unencoded = "mod % 3"
+      @uri_http_encoded = "%6d%6f%64%20%25%20%33"
+    end
+
+    it "should format each byte of the String" do
+      @uri_unencoded.format_http.should == @uri_http_encoded
+    end
   end
 end
