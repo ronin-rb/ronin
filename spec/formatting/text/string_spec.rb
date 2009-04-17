a95b90a08ea90721f5a8712c9bcf47eb1fc0734a
postmodern <postmodern.mod3@gmail.com>
2009-04-16 19:42:18 -0700

    Restructed the specs for the text formatting extensions.
    * Improved specs for String#random_case.

--- spec/formatting/text/string_spec.rb
+++ spec/formatting/text/string_spec.rb
@@ -11,30 +11,41 @@ describe String do
     @string.respond_to?('format_chars').should == true
   end
 
-  it "String#format_chars should format each character in the String" do
-    @string.format_chars { |c|
-      "_#{c}"
-    }.should == "_h_e_l_l_o"
-  end
-
   it "should provide String#format_bytes" do
     @string.respond_to?('format_bytes').should == true
-  end
-
-  it "String#format_bytes should format each byte in the String" do
-    @string.format_bytes { |b|
-      sprintf("%%%x",b)
-    }.should == "%68%65%6c%6c%6f"
   end
 
   it "should provide String#random_case" do
     @string.respond_to?('random_case').should == true
   end
 
-  it "String#random_case should randomly capitalize each character" do
-    new_string = @string.random_case
+  describe "format_chars" do
+    it "should format each character in the String" do
+      @string.format_chars { |c|
+        "_#{c}"
+      }.should == "_h_e_l_l_o"
+    end
+  end
 
-    new_string.should_not == @string
-    new_string.downcase.should == @string
+  describe "format_bytes" do
+    it "should format each byte in the String" do
+      @string.format_bytes { |b|
+        sprintf("%%%x",b)
+      }.should == "%68%65%6c%6c%6f"
+    end
+  end
+
+  describe "random_case" do
+    it "should capitalize each character when :probability is 1.0" do
+      new_string = @string.random_case(:probability => 1.0)
+
+      @string.upcase.should == new_string
+    end
+
+    it "should not capitalize any characters when :probability is 0.0" do
+      new_string = @string.random_case(:probability => 0.0)
+
+      @string.should == new_string
+    end
   end
 end
