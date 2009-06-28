421d139e0e457e61bee84851c0df3851cdce80ad
postmodern <postmodern.mod3@gmail.com>
2009-06-27 17:05:50 -0700

    Fixed failing specs.

--- spec/scanners/scanner_spec.rb
+++ spec/scanners/scanner_spec.rb
@@ -20,10 +20,12 @@ describe Scanners::Scanner do
 
   it "should separate scanner tests between inherited classes" do
     ExampleScanner.scanners[:test1].length.should == 1
+    ExampleScanner.scanners[:fail].length.should == 1
     ExampleScanner.scanners[:test3].should be_nil
 
     AnotherScanner.scanners[:test1].length.should == 1
     AnotherScanner.scanners[:test3].length.should == 1
+    AnotherScanner.scanners[:fail].length.should == 1
   end
 
   it "should specify if the scanner defines a certain category of tests" do
@@ -39,22 +41,27 @@ describe Scanners::Scanner do
   end
 
   it "should specify the category names of all tests" do
-    ExampleScanner.scans_for.should == Set[:test1, :test2]
-    AnotherScanner.scans_for.should == Set[:test1, :test2, :test3]
+    ExampleScanner.scans_for.should == Set[:test1, :test2, :fail]
+    AnotherScanner.scans_for.should == Set[:test1, :test2, :test3, :fail]
   end
 
   it "should scan each target and return the discovered results" do
-    @example_scanner.scan.should == {:test1 => [1], :test2 => [2, 4]}
+    @example_scanner.scan.should == {
+      :test1 => [1],
+      :test2 => [2, 4],
+      :fail => []
+    }
 
     @another_scanner.scan.should == {
       :test1 => [1, 1],
       :test2 => [2, 4],
-      :test3 => [3]
+      :test3 => [3],
+      :fail => []
     }
   end
 
   it "should scan each target and pass back results" do
-    results = {:test1 => [1], :test2 => [2, 2]}
+    results = {:test1 => [1], :test2 => [2, 4], :fail => []}
 
     @example_scanner.scan do |category,result|
       results.has_key?(category).should == true
@@ -71,12 +78,16 @@ describe Scanners::Scanner do
     }
   end
 
+  it "should return an empty Array for a failed scan" do
+    @example_scanner.scan(:fail => true).should == {:fail => []}
+  end
+
   it "should not scan a category if the category information is false" do
-    @example_scanner.scan(:test1 => false).should be_empty
+    @example_scanner.scan(:test1 => false).should == {}
   end
 
   it "should not scan a category if the category information is nil" do
-    @example_scanner.scan(:test1 => nil).should be_empty
+    @example_scanner.scan(:test1 => nil).should == {}
   end
 
   it "should raise an UnknownCategory for unknown scan categories" do
@@ -99,7 +110,15 @@ describe Scanners::Scanner do
     end
   end
 
+  it "should return an empty Array for failed convenience scans" do
+    @example_scanner.fail_scan.should == []
+  end
+
   it "should define a convenience method for returning the first result" do
     @example_scanner.get_test2.should == 2
+  end
+
+  it "should return nil when there is no first result to return" do
+    @example_scanner.get_fail.should be_nil
   end
 end
