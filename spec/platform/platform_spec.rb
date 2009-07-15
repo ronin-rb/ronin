245a7953ece70e165d12a5dc5e7fa432d420000b
postmodern <postmodern.mod3@gmail.com>
2009-07-15 14:51:56 -0700

    Added spes for Platform.extension_names and Platform.has_extensions?.

--- spec/platform/platform_spec.rb
+++ spec/platform/platform_spec.rb
@@ -11,4 +11,12 @@ describe Platform do
   it "should be able to load custom overlay caches" do
     Platform.overlays.should_not be_empty
   end
+
+  it "should have specific extensions" do
+    Platform.has_extension?('test').should == true
+  end
+
+  it "should provide the names of all available extensions" do
+    Platform.extension_names.should == ['hello', 'random', 'test']
+  end
 end
