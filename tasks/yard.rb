963e7917da937008d63cd5ca47504137cd775eaf
postmodern <postmodern.mod3@gmail.com>
2009-08-10 14:25:58 -0700

    Added the --files option.

--- tasks/yard.rb
+++ tasks/yard.rb
@@ -2,5 +2,10 @@ require 'yard'
 
 YARD::Rake::YardocTask.new do |t|
   t.files   = ['lib/**/*.rb', 'History.txt']
-  t.options = ['--quiet', '--protected', '--title', 'Ronin']
+  t.options = [
+    '--protected',
+    '--files', 'COPYING.txt',
+    '--title', 'Ronin',
+    '--quiet'
+  ]
 end
