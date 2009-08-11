78d95fb354deb27e52baff111d8876b3b3c4c675
postmodern <postmodern.mod3@gmail.com>
2009-08-10 19:41:12 -0700

    Added 'ronin/yard' to contain ronin specific documentation handlers.

---
+++ lib/ronin/yard/handlers/ruby/legacy/eval_block.rb
@@ -0,0 +1,19 @@
+require 'yard/handlers/ruby/legacy/base'
+
+module YARD
+  module Handlers
+    module Ruby
+      module Legacy
+        class EvalBlock < Base
+
+          handles /(\A|\.)(module|class|instance)_eval(\s+|\()/
+
+          def process
+            parse_block if statement.block
+          end
+
+        end
+      end
+    end
+  end
+end
--- tasks/yard.rb
+++ tasks/yard.rb
@@ -1,5 +1,12 @@
 require 'yard'
 
+lib_dir = File.expand_path(File.join(File.dirname(__FILE__),'..','lib'))
+unless $LOAD_PATH.include?(lib_dir)
+  $LOAD_PATH << lib_dir
+end
+
+require 'ronin/yard/handlers'
+
 YARD::Rake::YardocTask.new do |t|
   t.files   = ['lib/**/*.rb']
   t.options = [
