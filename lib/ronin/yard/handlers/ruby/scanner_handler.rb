fd2efbb6777a15bd74504eab39f88eb848e42f89
postmodern <postmodern.mod3@gmail.com>
2009-08-12 18:06:40 -0700

    Only detect method_call(:scanner).
    * There is no :scanner keyword.

--- lib/ronin/yard/handlers/ruby/scanner_handler.rb
+++ lib/ronin/yard/handlers/ruby/scanner_handler.rb
@@ -28,22 +28,17 @@ module YARD
     module Ruby
       class ScannerHandler < Base
 
-        handles :scanner, method_call(:scanner)
+        handles method_call(:scanner)
 
         def process
+          obj = statement.parameters(false).first
           nobj = namespace
           mscope = scope
-          name = if statement.type == :scanner
-                   statement.jump(:ident, :op, :kw, :const).source
-                 elsif statement.call?
-                   obj = statement.parameters(false).first
-
-                   case obj.type
-                   when :symbol_literal
-                     obj.jump(:ident, :op, :kw, :const).source
-                   when :string_literal
-                     obj.jump(:string_content).source
-                   end
+          name = case obj.type
+                 when :symbol_literal
+                   obj.jump(:ident, :op, :kw, :const).source
+                 when :string_literal
+                   obj.jump(:string_content).source
                  end
 
           register MethodObject.new(nobj, "first_#{name}", mscope) do |o|
