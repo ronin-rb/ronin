004b6eb65750982a345c0662f834bb30e5625d1e
postmodern <postmodern.mod3@gmail.com>
2009-08-12 18:19:59 -0700

    Unshift the lib/ directory onto $LOAD_PATH.

--- tasks/yard.rb
+++ tasks/yard.rb
@@ -1,6 +1,6 @@
 lib_dir = File.expand_path(File.join(File.dirname(__FILE__),'..','lib'))
 unless $LOAD_PATH.include?(lib_dir)
-  $LOAD_PATH << lib_dir
+  $LOAD_PATH.unshift(lib_dir)
 end
 
 require 'yard'
