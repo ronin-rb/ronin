6e995ae2a173c0f97f3210b14cc4df64744945fd
postmodern <postmodern.mod3@gmail.com>
2009-08-12 20:36:27 -0700

    Removed -- and ++ RDoc syntax.

--- lib/ronin/formatting/digest.rb
+++ lib/ronin/formatting/digest.rb
@@ -1,5 +1,4 @@
 #
-#--
 # Ronin - A Ruby platform designed for information security and data
 # exploration tasks.
 #
@@ -18,7 +17,6 @@
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-#++
 #
 
 require 'ronin/formatting/extensions/digest'
