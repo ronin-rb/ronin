729fa503f9cf4ba4343dacac3a78755132395e73
postmodern <postmodern.mod3@gmail.com>
2009-08-27 19:17:21 -0700

    Fixed indentation.

--- lib/ronin/extensions/ip_addr.rb
+++ lib/ronin/extensions/ip_addr.rb
@@ -1,16 +1,16 @@
-  #
-  # Ronin - A Ruby platform designed for information security and data
-  # exploration tasks.
-  #
-  # Copyright (c) 2006-2009 Hal Brodigan (postmodern.mod3 at gmail.com)
-  #
-  # This program is free software; you can redistribute it and/or modify
-  # it under the terms of the GNU General Public License as published by
-  # the Free Software Foundation; either version 2 of the License, or
-  # (at your option) any later version.
-  #
-  # This program is distributed in the hope that it will be useful,
-  # but WITHOUT ANY WARRANTY; without even the implied warranty of
+#
+# Ronin - A Ruby platform designed for information security and data
+# exploration tasks.
+#
+# Copyright (c) 2006-2009 Hal Brodigan (postmodern.mod3 at gmail.com)
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
