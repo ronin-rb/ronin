be3653e83eab0cfe05f68ad06b5725cbd496a172
postmodern <postmodern.mod3@gmail.com>
2009-09-19 21:18:46 -0700

    Use statement.parameters and make sure the first argument of belongs_to is a Symbol.

--- lib/ronin/yard/handlers/ruby/belongs_to_handler.rb
+++ lib/ronin/yard/handlers/ruby/belongs_to_handler.rb
@@ -12,26 +12,30 @@ module YARD
         def process
           nobj = effected_namespace
           mscope = scope
-          name = statement.jump(:symbol).source[1..-1]
+          name = statement.parameters[0].last
+          
+          if name.type == :symbol
+            name = name.source[1..-1]
 
-          register MethodObject.new(nobj, name, :class) do |o|
-            o.visibility = :public
-            o.source = statement.source
-            o.signature = "def self.#{name}"
-            o.parameters = [['repository', 'nil']]
-          end
+            register MethodObject.new(nobj, name, :class) do |o|
+              o.visibility = :public
+              o.source = statement.source
+              o.signature = "def self.#{name}"
+              o.parameters = [['repository', 'nil']]
+            end
 
-          register MethodObject.new(nobj, name, mscope) do |o|
-            o.visibility = :public
-            o.source = statement.source
-            o.signature = "def #{name}"
-          end
+            register MethodObject.new(nobj, name, mscope) do |o|
+              o.visibility = :public
+              o.source = statement.source
+              o.signature = "def #{name}"
+            end
 
-          register MethodObject.new(nobj, "#{name}=", mscope) do |o|
-            o.visibility = :public
-            o.source = statement.source
-            o.signature = "def #{name}=(resource)"
-            o.parameters = [['resource', nil]]
+            register MethodObject.new(nobj, "#{name}=", mscope) do |o|
+              o.visibility = :public
+              o.source = statement.source
+              o.signature = "def #{name}=(resource)"
+              o.parameters = [['resource', nil]]
+            end
           end
         end
 
