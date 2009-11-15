f9b1846a5c6bd1cec95024b6c034bcf7e70ae919
postmodern <postmodern.mod3@gmail.com>
2009-11-15 02:17:50 -0800

    Fixed a spec that was failing to load.

--- spec/model/lazy_setup_spec.rb
+++ spec/model/lazy_setup_spec.rb
@@ -1,9 +1,9 @@
-require 'ronin/model/lazy_upgrade'
+require 'ronin/model/lazy_setup'
 
 require 'spec_helper'
 require 'model/classes/lazy_model'
 
-describe Ronin::Model::LazyUpgrade do
+describe Ronin::Model::LazySetup do
   it "should not be auto_upgrade after being loaded" do
     LazyModel.should_not be_auto_upgraded
   end
