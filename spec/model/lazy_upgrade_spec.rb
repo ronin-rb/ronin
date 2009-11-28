913431624a913e347462fdd4e1a34892b5f6dd66
postmodern <postmodern.mod3@gmail.com>
2009-11-28 11:48:21 -0800

    Added more specs for Ronin::Model::LazyUpgrade.

--- spec/model/lazy_upgrade_spec.rb
+++ spec/model/lazy_upgrade_spec.rb
@@ -4,7 +4,17 @@ require 'spec_helper'
 require 'model/classes/lazy_model'
 
 describe Ronin::Model::LazySetup do
-  it "should not be auto_upgrade after being loaded" do
-    LazyModel.should_not be_auto_upgraded
+  describe "pre auto_upgrade" do
+    it "should not be auto_upgraded after being loaded" do
+      LazyModel.should_not be_auto_upgraded
+    end
+  end
+
+  describe "post auto_upgrade" do
+    it "should be auto_upgraded after lazy_upgrade!" do
+      LazyModel.lazy_upgrade!
+
+      LazyModel.should be_auto_upgraded
+    end
   end
 end
