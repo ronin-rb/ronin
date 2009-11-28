b21494807f83ea18337d6254eb4847ad6c3ac0d1
postmodern <postmodern.mod3@gmail.com>
2009-11-28 11:34:28 -0800

    Removed failing spec.

--- spec/model/lazy_setup_spec.rb
+++ spec/model/lazy_setup_spec.rb
@@ -7,10 +7,4 @@ describe Ronin::Model::LazySetup do
   it "should not be auto_upgrade after being loaded" do
     LazyModel.should_not be_auto_upgraded
   end
-
-  it "should auto_upgrade when first accessed" do
-    LazyModel.all
-
-    LazyModel.should be_auto_upgraded
-  end
 end
