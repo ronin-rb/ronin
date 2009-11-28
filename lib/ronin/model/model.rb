612b4767a1557047382d89acfaec687e5b2a49ce
postmodern <postmodern.mod3@gmail.com>
2009-11-28 11:42:44 -0800

    Renamed 'lib/ronin/model/lazy_setup.rb' to 'lib/ronin/model/lazy_upgrade.rb'.

--- lib/ronin/model/model.rb
+++ lib/ronin/model/model.rb
@@ -18,7 +18,7 @@
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #
 
-require 'ronin/model/lazy_setup'
+require 'ronin/model/lazy_upgrade'
 
 require 'extlib'
 require 'dm-core'
