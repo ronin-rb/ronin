ddbd69932a1258e489a06e56fa011488d1362cf3
postmodern <postmodern.mod3@gmail.com>
2009-11-28 11:49:33 -0800

    Added some specs for Ronin::Database.

---
+++ spec/database_spec.rb
@@ -0,0 +1,29 @@
+require 'ronin/database'
+
+require 'spec_helper'
+
+describe Database do
+  it "should auto_upgrade the Author model" do
+    Author.should be_auto_upgraded
+  end
+
+  it "should auto_upgrade the License model" do
+    License.should be_auto_upgraded
+  end
+
+  it "should auto_upgrade the Arch model" do
+    Arch.should be_auto_upgraded
+  end
+
+  it "should auto_upgrade the OS model" do
+    OS.should be_auto_upgraded
+  end
+
+  it "should auto_upgrade the Product model" do
+    Product.should be_auto_upgraded
+  end
+
+  it "should auto_upgrade the Cacheable::CachedFile model" do
+    Cacheable::CachedFile.should be_auto_upgraded
+  end
+end
