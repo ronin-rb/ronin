3d2f5fff5d8c0ca7fb73e99da80a16e8302f75a9
postmodern <postmodern.mod3@gmail.com>
2010-01-13 20:20:43 -0800

    Remove the spec.rb file, now that ronin-gen can add it to generated overlays.

--- lib/ronin/platform/tasks/spec.rb
+++
@@ -1,9 +0,0 @@
-require 'spec/rake/spectask'
-
-desc "Run all specifications"
-Spec::Rake::SpecTask.new(:spec) do |t|
-  t.libs += ['lib'] + Dir['**/lib'] + ['spec']
-  t.spec_opts = ['--colour', '--format', 'specdoc']
-end
-
-task :default => :spec
