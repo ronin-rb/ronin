2dba7692ac166afd7835cef23785eb2587aecfd3
postmodern <postmodern.mod3@gmail.com>
2010-02-03 22:38:11 -0800

    Remove unnecessary requirements.

--- lib/ronin/arch.rb
+++ lib/ronin/arch.rb
@@ -18,8 +18,6 @@
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #
 
-require 'ronin/extensions/meta'
-require 'ronin/extensions/string'
 require 'ronin/model'
 
 require 'dm-predefined'
