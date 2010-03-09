53fd5b089ec7de4472f0f492c500d1ec9349dd53
postmodern <postmodern.mod3@gmail.com>
2010-03-08 17:34:24 -0800

    Removed an old spec.

--- spec/ronin_spec.rb
+++ spec/ronin_spec.rb
@@ -1,4 +1,3 @@
-require 'ronin/ronin'
 require 'ronin/version'
 
 require 'spec_helper'
@@ -8,9 +7,5 @@ describe Ronin do
     @version = Ronin.const_get('VERSION')
     @version.should_not be_nil
     @version.should_not be_empty
-  end
-
-  it "should be able to find files within Ronin" do
-    Ronin.find_files(File.join('lib','ronin','version.rb')).should_not be_empty
   end
 end
