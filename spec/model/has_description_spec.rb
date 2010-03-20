5b6e7c3dd08d4c90807118441495b27b9cec25bf
postmodern <postmodern.mod3@gmail.com>
2010-03-19 20:33:14 -0700

    Added a spec for Model::HasDescription.describing.

--- spec/model/has_description_spec.rb
+++ spec/model/has_description_spec.rb
@@ -8,39 +8,49 @@ describe Model::HasDescription do
     DescribedModel.auto_migrate!
   end
 
-  before(:each) do
-    @model = DescribedModel.new
+  describe "description" do
+    before(:each) do
+      @model = DescribedModel.new
+    end
+
+    it "should allow the setting of the description" do
+      @model.description = 'test one'
+      @model.description.should == 'test one'
+    end
+
+    it "should strip leading and tailing white-space" do
+      @model.description = %{   test two    }
+
+      @model.description.should == 'test two'
+    end
+
+    it "should strip leading and tailing white-space from each line" do
+      @model.description = %{
+        test
+        three
+      }
+
+      @model.description.should == "test\nthree"
+    end
+
+    it "should preserve non-bordering empty lines" do
+      @model.description = %{
+        test
+
+        four
+      }
+
+      @model.description.should == "test\n\nfour"
+    end
   end
 
-  it "should allow the setting of the description" do
-    @model.description = 'test one'
-    @model.description.should == 'test one'
-  end
+  it "should be able to find resources with similar descriptions" do
+    DescribedModel.create!(:description => 'foo one')
+    DescribedModel.create!(:description => 'foo bar two')
 
-  it "should strip leading and tailing white-space" do
-    @model.description = %{
-test two
-    }
-
-    @model.description.should == 'test two'
-  end
-
-  it "should strip leading and tailing white-space from each line" do
-    @model.description = %{
-      test
-      three
-    }
-
-    @model.description.should == "test\nthree"
-  end
-
-  it "should preserve non-bordering empty lines" do
-    @model.description = %{
-      test
-
-      four
-    }
-
-    @model.description.should == "test\n\nfour"
+    models = DescribedModel.describing('foo')
+    models.length.should == 2
+    models[0].description.should == 'foo one'
+    models[1].description.should == 'foo bar two'
   end
 end
