6ea37a44aa496c87adc78b3bff30ceab29db6b9f
postmodern <postmodern.mod3@gmail.com>
2010-03-20 20:55:21 -0700

    Create each Overlay individually for the specs.

--- spec/platform/helpers/overlays.rb
+++ spec/platform/helpers/overlays.rb
@@ -4,9 +4,23 @@ module Helpers
   module Overlays
     OVERLAYS_DIR = File.expand_path(File.join(File.dirname(__FILE__),'overlays'))
 
-    Dir[File.join(OVERLAYS_DIR,'*')].each do |path|
-      Platform::Overlay.create!(:path => path)
-    end
+    Platform::Overlay.create!(
+      :path => File.join(OVERLAYS_DIR,'hello')
+    )
+
+    Platform::Overlay.create!(
+      :path => File.join(OVERLAYS_DIR,'random'),
+      :uri => 'git@github.com:/path/to/random.git',
+      :installed => true
+    )
+
+    Platform::Overlay.create!(
+      :path => File.join(OVERLAYS_DIR,'test1')
+    )
+
+    Platform::Overlay.create!(
+      :path => File.join(OVERLAYS_DIR,'test2')
+    )
 
     def load_overlay(name)
       Platform::Overlay.first(:name => name)
