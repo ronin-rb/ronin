03302095bf3113f79374356171f1150f6bb47e59
postmodern <postmodern.mod3@gmail.com>
2010-03-20 21:30:13 -0700

    Explicitly specify all of the important properties for the spec Overlays.

--- spec/platform/helpers/overlays.rb
+++ spec/platform/helpers/overlays.rb
@@ -5,23 +5,31 @@ module Helpers
     OVERLAYS_DIR = File.expand_path(File.join(File.dirname(__FILE__),'overlays'))
 
     Platform::Overlay.create!(
-      :path => File.join(OVERLAYS_DIR,'hello')
+      :path => File.join(OVERLAYS_DIR,'hello'),
+      :name => 'hello',
+      :host => Platform::Overlay::DEFAULT_HOST
     )
 
     Platform::Overlay.create!(
       :path => File.join(OVERLAYS_DIR,'random'),
       :uri => 'git@github.com/path/to/random.git',
-      :installed => true
+      :installed => true,
+      :name => 'random',
+      :host => 'github.com'
     )
 
     Platform::Overlay.create!(
       :path => File.join(OVERLAYS_DIR,'test1'),
-      :uri => 'git@github.com/path/to/test1.git'
+      :uri => 'git@github.com/path/to/test1.git',
+      :name => 'test1',
+      :host => 'github.com'
     )
 
     Platform::Overlay.create!(
       :path => File.join(OVERLAYS_DIR,'test2'),
-      :uri => 'git@github.com/path/to/test2.git'
+      :uri => 'git@github.com/path/to/test2.git',
+      :name => 'test2',
+      :host => 'github.com'
     )
 
     def load_overlay(name)
