a53b805463b7b89f430b094fd158bf44ecfa6bcb
postmodern <postmodern.mod3@gmail.com>
2010-03-31 21:13:42 -0700

    Raise the exception directly in the context block.

--- spec/platform/helpers/overlays/test1/cache/cacheable_model/exceptions.rb
+++ spec/platform/helpers/overlays/test1/cache/cacheable_model/exceptions.rb
@@ -2,8 +2,6 @@ require 'platform/classes/cacheable_model'
 
 ronin_cacheable_model do
 
-  cache do
-    raise("Exception when caching a file")
-  end
+  raise("Exception when caching a file")
 
 end
