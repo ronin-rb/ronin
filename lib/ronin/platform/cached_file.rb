de7096b4012df193707656a9f039191dfb6afb18
postmodern <postmodern.mod3@gmail.com>
2010-04-02 04:32:31 -0700

    Catch all exceptions when loading a fresh object from a cached file.

--- lib/ronin/platform/cached_file.rb
+++ lib/ronin/platform/cached_file.rb
@@ -125,7 +125,7 @@ module Ronin
         begin
           # load the first found context
           blocks = Contextify.load_blocks(self.path)
-        rescue SyntaxError, LoadError, NoMethodError => e
+        rescue Exception => e
           @cache_exception = e
           return nil
         end
