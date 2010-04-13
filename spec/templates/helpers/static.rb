f4d7ce758d8957ba363111d9e83451604c1d3f98
postmodern <postmodern.mod3@gmail.com>
2010-04-13 16:33:07 -0700

    Removed the old Template specs, those are now in ronin-ext.

--- spec/templates/helpers/static.rb
+++
@@ -1,7 +0,0 @@
-require 'ronin/config'
-
-module Helpers
-  STATIC_TEMPLATE_DIR = File.expand_path(File.join(File.dirname(__FILE__),'static'))
-
-  Ronin::Config.register_static_dir STATIC_TEMPLATE_DIR
-end
