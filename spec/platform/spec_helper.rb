cacc48aeddaf16a71e9337dc8b9ece4175c5cce5
Postmodern <postmodern.mod3@gmail.com>
2010-06-25 00:48:47 -0700

    Fixed a constant scope issue for Ruby 1.9.2.

--- spec/platform/spec_helper.rb
+++ spec/platform/spec_helper.rb
@@ -7,13 +7,13 @@ Spec::Runner.configure do |spec|
 
   spec.before(:suite) do
     Platform::Overlay.create!(
-      :path => File.join(OVERLAYS_DIR,'hello'),
+      :path => File.join(Helpers::Overlays::OVERLAYS_DIR,'hello'),
       :name => 'hello',
       :domain => Platform::Overlay::LOCAL_DOMAIN
     )
 
     Platform::Overlay.create!(
-      :path => File.join(OVERLAYS_DIR,'random'),
+      :path => File.join(Helpers::Overlays::OVERLAYS_DIR,'random'),
       :uri => 'git@github.com/path/to/random.git',
       :installed => true,
       :name => 'random',
@@ -21,14 +21,14 @@ Spec::Runner.configure do |spec|
     )
 
     Platform::Overlay.create!(
-      :path => File.join(OVERLAYS_DIR,'test1'),
+      :path => File.join(Helpers::Overlays::OVERLAYS_DIR,'test1'),
       :uri => 'git@github.com/path/to/test1.git',
       :name => 'test1',
       :domain => 'github.com'
     )
 
     Platform::Overlay.create!(
-      :path => File.join(OVERLAYS_DIR,'test2'),
+      :path => File.join(Helpers::Overlays::OVERLAYS_DIR,'test2'),
       :uri => 'git@github.com/path/to/test2.git',
       :name => 'test2',
       :domain => 'github.com'
