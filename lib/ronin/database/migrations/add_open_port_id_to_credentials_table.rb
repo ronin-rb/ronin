84fb6f6174980e39c3ef3ea1967f4d0d9b5a5282
Postmodern <postmodern.mod3@gmail.com>
2010-06-25 20:53:45 -0700

    Add open_port_id to ronin_credentials table.

---
+++ lib/ronin/database/migrations/add_open_port_id_to_credentials_table.rb
@@ -0,0 +1,41 @@
+#
+# Ronin - A Ruby platform for exploit development and security research.
+#
+# Copyright (c) 2006-2010 Hal Brodigan (postmodern.mod3 at gmail.com)
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+#
+
+require 'ronin/database/migrations/migrations'
+
+module Ronin
+  module Database
+    module Migrations
+      migration(:ronin, '0.4.0', :add_open_port_id_column_to_credentials_table) do
+        up do
+          modify_table :ronin_credentials do
+            add_column :open_port_id, Integer
+          end
+        end
+
+        down do
+          modify_table :ronin_credentials do
+            remove_column :open_port_id
+          end
+        end
+      end
+    end
+  end
+end
