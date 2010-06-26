a36b065d1bc5467331686441f5952168c7731e69
Postmodern <postmodern.mod3@gmail.com>
2010-06-25 17:27:04 -0700

    Added a migration for the IPAddressHostName join-model.

--- lib/ronin/database/migrations/create_host_name_ip_addresses_table.rb
+++ lib/ronin/database/migrations/create_host_name_ip_addresses_table.rb
@@ -19,15 +19,23 @@
 #
 
 require 'ronin/database/migrations/migrations'
-require 'ronin/database/migrations/create_arches_table'
-require 'ronin/database/migrations/create_oses_table'
-require 'ronin/database/migrations/create_vendors_table'
-require 'ronin/database/migrations/create_softwares_table'
-require 'ronin/database/migrations/create_licenses_table'
-require 'ronin/database/migrations/create_authors_table'
-require 'ronin/database/migrations/create_addresses_table'
-require 'ronin/database/migrations/create_ports_table'
-require 'ronin/database/migrations/add_version_column_to_addresses_table'
-require 'ronin/database/migrations/create_ip_address_mac_addresses_table'
-require 'ronin/database/migrations/create_host_name_ip_addresses_table'
-require 'ronin/database/migrations/platform'
+
+module Ronin
+  module Database
+    module Migrations
+      migration(:ronin, '0.4.0', :create_host_name_ip_addresses_table) do
+        up do
+          create_table :ronin_host_name_ip_addresses do
+            column :host_name_id, Integer, :key => true
+            column :ip_address_id, Integer, :key => true
+            column :created_at, Time, :not_null => true
+          end
+        end
+
+        down do
+          drop_table :ronin_host_name_ip_addresses
+        end
+      end
+    end
+  end
+end
