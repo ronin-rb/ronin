7e11252ea5be106b216e6bcb15ff307ca677f7b0
Postmodern <postmodern.mod3@gmail.com>
2010-08-13 02:43:37 -0700

    More use of 'subject' in specs.

--- spec/ronin_spec.rb
+++ spec/ronin_spec.rb
@@ -3,8 +3,9 @@ require 'ronin/version'
 
 describe Ronin do
   it "should have a version" do
-    @version = Ronin.const_get('VERSION')
-    @version.should_not be_nil
-    @version.should_not be_empty
+    version = subject.const_get('VERSION')
+
+    version.should_not be_nil
+    version.should_not be_empty
   end
 end
