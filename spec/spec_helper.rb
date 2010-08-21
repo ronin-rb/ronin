64735f70151a5fcfc24df8b4bd5ccff3a41065f2
Postmodern <postmodern.mod3@gmail.com>
2010-08-20 23:08:49 -0700

    Bundle the :default group now.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,7 +2,7 @@ require 'rubygems'
 require 'bundler'
 
 begin
-  Bundler.setup(:runtime, :test)
+  Bundler.setup(:default, :test)
 rescue Bundler::BundlerError => e
   STDERR.puts e.message
   STDERR.puts "Run `bundle install` to install missing gems"
