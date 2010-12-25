6258160f402135fdea34656b69457fe9584d51b3
Postmodern <postmodern.mod3@gmail.com>
2010-12-24 17:23:38 -0800

    Renamed spec/helpers/repos/ to match ~/.ronin/repos/.

--- spec/helpers/repositories.rb
+++ spec/helpers/repositories.rb
@@ -2,7 +2,7 @@ require 'ronin/repository'
 
 module Helpers
   module Repositories
-    DIR = File.expand_path(File.join(File.dirname(__FILE__),'repositories'))
+    DIR = File.expand_path(File.join(File.dirname(__FILE__),'repos'))
 
     def repository(name)
       Repository.first(:name => name)
