aab6ca3b733acd3b32d253e601b65ae3f347b8d5
Postmodern <postmodern.mod3@gmail.com>
2010-12-25 21:55:32 -0800

    Added the ronin-emails command for querying EmailAddress.

---
+++ lib/ronin/ui/cli/commands/emails.rb
@@ -0,0 +1,70 @@
+#
+# Ronin - A Ruby platform for exploit development and security research.
+#
+# Copyright (c) 2006-2010 Hal Brodigan (postmodern.mod3 at gmail.com)
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+#
+
+require 'ronin/ui/cli/model_command'
+require 'ronin/email_address'
+
+module Ronin
+  module UI
+    module CLI
+      module Commands
+        #
+        # The `ronin-emails` command.
+        #
+        class Emails < ModelCommand
+
+          self.model = EmailAddress
+
+          query_option :hosts, :type => :array, :aliases => '-H' do |emails,hosts|
+            emails.all('host_name.address' => hosts)
+          end
+
+          query_option :ip, :type => :array, :aliases => '-I' do |emails,ips|
+            hosts.all('host_name.ip_addresses.address' => ips)
+          end
+
+          query_option :ports, :type => :array, :aliases => '-p' do |emails,ports|
+            emails.all('host_name.ports.number' => ports)
+          end
+
+          query_option :users, :type => :array, :aliases => '-u' do |emails,users|
+            emails.all('user_name.name' => users)
+          end
+
+          class_option :list, :type => :boolean,
+                              :default => true,
+                              :aliases => '-l'
+
+          #
+          # Queries the {EmailAddress} model.
+          #
+          # @since 1.0.0
+          #
+          def execute
+            if options.list?
+              super
+            end
+          end
+
+        end
+      end
+    end
+  end
+end
