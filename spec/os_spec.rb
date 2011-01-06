f1d84711ba79c0305901eeed2f91efe542168c8c
Postmodern <postmodern.mod3@gmail.com>
2011-01-05 18:14:27 -0800

    Added more specs to OS.

--- spec/os_spec.rb
+++ spec/os_spec.rb
@@ -2,6 +2,10 @@ require 'spec_helper'
 require 'ronin/os'
 
 describe OS do
+  subject do
+    OS.new(:name => 'Linux', :version => '2.6.11')
+  end
+
   it "should require a name" do
     os = OS.new
     os.should_not be_valid
@@ -16,5 +20,27 @@ describe OS do
 
   it "should provide methods for creating OSes with versions" do
     OS.linux('2.6.11').should be_valid
+  end
+
+  describe "#to_s" do
+    it "should convert both the name and version" do
+      os = OS.new(:name => 'Linux', :version => '2.6.23')
+
+      os.to_s.should == 'Linux 2.6.23'
+    end
+
+    it "should convert just the name if there is no version" do
+      os = OS.new(:name => 'Linux')
+
+      os.to_s.should == os.name
+    end
+  end
+
+  it "should implicitly splat the name and version" do
+    os = OS.new(:name => 'Linux', :version => '2.6.23')
+    name, version = os
+
+    name.should == os.name
+    version.should == os.version
   end
 end
