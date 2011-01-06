d3fd73bb8f0906e1c4a674e17daa8dd4d7d75360
Postmodern <postmodern.mod3@gmail.com>
2011-01-05 18:44:52 -0800

    Added some specs to Database.

--- spec/database_spec.rb
+++ spec/database_spec.rb
@@ -2,4 +2,27 @@ require 'spec_helper'
 require 'ronin/database'
 
 describe Database do
+  describe "repositories" do
+    it "should not be empty" do
+      subject.repositories.should_not be_empty
+    end
+
+    it "should have a ':default' repository" do
+      subject.repositories[:default].should_not be_nil
+    end
+  end
+
+  it "shold determine if a repository is defined" do
+    subject.repository?(:default).should == true
+  end
+
+  it "should determine when a repository is setup" do
+    subject.setup?(:default).should == true
+  end
+
+  it "should not allow switching to unknown repositories" do
+    lambda {
+      subject.repository(:foo) { }
+    }.should raise_error(Database::UnknownRepository)
+  end
 end
