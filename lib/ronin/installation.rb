323a642fb9ceb6cfe04d70a4ea868d63571e18fd
Postmodern <postmodern.mod3@gmail.com>
2011-03-20 23:12:57 -0400

    No need to require 'enumerator'.

--- lib/ronin/installation.rb
+++ lib/ronin/installation.rb
@@ -17,8 +17,6 @@
 # along with Ronin.  If not, see <http://www.gnu.org/licenses/>.
 #
 
-require 'enumerator'
-
 module Ronin
   #
   # The {Installation} module provides methods which help reflect on the
