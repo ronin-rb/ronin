23dd0d01b79e678b7aa1b90573dd196e226f1ab2
Postmodern <postmodern.mod3@gmail.com>
2011-06-25 01:24:40 -0700

    Fixed a spec that relied on example.com's IP never changing.

--- spec/host_name_spec.rb
+++ spec/host_name_spec.rb
@@ -2,10 +2,10 @@ require 'spec_helper'
 require 'ronin/host_name'
 
 describe HostName do
-  let(:example_domain) { 'www.example.com' }
-  let(:example_ip) { '192.0.32.10' }
+  let(:domain) { 'localhost' }
+  let(:ip) { '127.0.0.1' }
 
-  subject { HostName.new(:address => example_domain) }
+  subject { HostName.new(:address => domain) }
 
   it "should require an address" do
     host_name = HostName.new
@@ -23,17 +23,16 @@ describe HostName do
     let(:bad_ip) { '0.0.0.0' }
 
     it "should look up the host names for an IP Address" do
-      host_names = subject.lookup(example_ip)
+      host_names = subject.lookup(ip).map { |name| name.address }
       
-      host_names.should_not be_empty
-      host_names[0].address.should == example_domain
+      host_names.should include(domain)
     end
 
     it "should associate the host names with the original IP address" do
-      host_names = subject.lookup(example_ip)
+      host_names = subject.lookup(ip)
       
       host_names.each do |host|
-        host.ip_addresses[0].address.should == example_ip
+        host.ip_addresses[0].address.should == ip
       end
     end
 
@@ -51,14 +50,14 @@ describe HostName do
       ips = subject.lookup!
       
       ips.should_not be_empty
-      ips[0].address.should == example_ip
+      ips[0].address.should == ip
     end
 
     it "should associate the IP addresses with the original host name" do
       ips = subject.lookup!
       
       ips.each do |ip|
-        ip.host_names[0].address.should == example_domain
+        ip.host_names[0].address.should == domain
       end
     end
 
