a59e7286e0e02780227117a6eaa2d188910d9ae4
Postmodern <postmodern.mod3@gmail.com>
2011-10-16 17:59:41 -0700

    Forgot to add MACAddress.extract.

--- lib/ronin/mac_address.rb
+++ lib/ronin/mac_address.rb
@@ -22,6 +22,8 @@ require 'ronin/address'
 require 'ronin/ip_address'
 require 'ronin/ip_address_mac_address'
 
+require 'strscan'
+
 module Ronin
   #
   # Represents MAC addresses that can be stored in the {Database}.
@@ -43,6 +45,37 @@ module Ronin
     # The IP Addresses associated with the MAC Address
     has 0..n, :ip_addresses, :through => :ip_address_mac_addresses,
                              :model => 'IPAddress'
+
+    #
+    # Extracts MAC addresses from the given text.
+    #
+    # @param [String] text
+    #   The text to parse.
+    #
+    # @yield [mac]
+    #   The given block will be passed each extracted MAC address.
+    #
+    # @yieldparam [MACAddress] mac
+    #   An extracted MAC Address
+    #
+    # @return [Array<MACAddress>]
+    #   If no block is given, an Array of MACAddress will be returned.
+    #
+    # @see 1.3.0
+    #
+    # @api public
+    #
+    def self.extract(text)
+      return enum_for(:extract,text).to_a unless block_given?
+
+      scanner = StringScanner.new(text)
+
+      while scanner.skip_until(Regexp::MAC)
+        yield parse(scanner.matched)
+      end
+
+      return nil
+    end
 
     #
     # The IP Address that most recently used the MAC Address.
--- spec/mac_address_spec.rb
+++ spec/mac_address_spec.rb
@@ -6,6 +6,26 @@ describe MACAddress do
 
   subject { MACAddress.new(:address => address) }
 
+  describe "extract" do
+    subject { described_class }
+
+    let(:mac1) { subject.parse('00:12:34:56:78:9a') }
+    let(:mac2) { subject.parse('AA:BB:CC:DD:EE:FF') }
+    let(:text)  { "MACs: #{mac1}, #{mac2}." }
+
+    it "should extract multiple MAC Addresses from text" do
+      subject.extract(text).should == [mac1, mac2]
+    end
+
+    it "should yield the extracted MAC Addresses if a block is given" do
+      macs = []
+
+      subject.extract(text) { |mac| macs << mac }
+
+      macs.should == [mac1, mac2]
+    end
+  end
+
   it "should require an address" do
     mac = MACAddress.new
 
