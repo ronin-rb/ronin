7c5f8c792f84e6feedd394a137aaac3526fff7a8
Postmodern <postmodern.mod3@gmail.com>
2011-11-21 23:00:31 -0800

    Changed the `ronin repos` command to only list repositories.

--- lib/ronin/ui/cli/commands/repos.rb
+++ lib/ronin/ui/cli/commands/repos.rb
@@ -29,224 +29,85 @@ module Ronin
         #
         class Repos < ModelCommand
 
-          desc 'Manages Ronin repositories'
-          class_option :add, :type => :string,
-                             :aliases => '-A',
-                             :banner => 'PATH'
-          class_option :install, :type => :string,
-                                 :aliases => '-I',
-                                 :banner => 'URI'
-          class_option :list, :type => :boolean,
-                              :aliases => '-l'
-          class_option :update, :type => :boolean,
-                                :aliases => '-u'
-          class_option :uninstall, :type => :string,
-                                   :aliases => '-U',
-                                   :banner => 'REPO'
-          class_option :scm, :type => :string, :aliases => '-S'
-
-          class_option :rsync, :type => :boolean
-          class_option :svn, :type => :boolean
-          class_option :hg, :type => :boolean
-          class_option :git, :type => :boolean
-
-          argument :name, :type => :string, :required => false
+          desc 'Lists Ronin Repositories'
+          argument :name, :type     => :string,
+                          :required => false
 
           #
           # Executes the command.
           #
           def execute
-            if options[:add]
-              add options[:add]
-            elsif options[:install]
-              install options[:install]
-            elsif options.update?
-              update
-            elsif options[:uninstall]
-              uninstall options[:uninstall]
+            if name
+              repository = begin
+                             Repository.find(name)
+                           rescue RepositoryNotFound => e
+                             print_error e.message
+                             exit -1
+                           end
+
+              print_repository(repository)
             else
-              list
+              print_array Repository.all
             end
           end
 
           protected
 
           #
-          # The selected SCM.
-          #
-          # @return [Symbol]
-          #   The name of the selected SCM.
-          #
-          def scm
-            @scm ||= if options[:scm]
-                       options[:scm].to_sym
-                     elsif options.rsync?
-                       :rsync
-                     elsif options.svn?
-                       :sub_version
-                     elsif options.hg?
-                       :mercurial
-                     elsif options.git?
-                       :git
-                     end
-          end
-
-          #
-          # Print out any exceptions or validation errors encountered
-          # when caching the files of the repository.
+          # Prints information about a Repository.
           #
           # @param [Repository] repo
-          #   The repository that was updated.
+          #   The repository to print.
           #
-          def print_cache_errors(repo)
-            repo.script_paths.each do |script_path|
-              if script_path.cache_exception
-                print_exception script_path.cache_exception
+          def print_repository(repo)
+            print_title repo.name
+
+            indent do
+              if repo.installed?
+                puts "Domain: #{repo.domain}"
+              else
+                puts "Path: #{repo.path}"
               end
 
-              if script_path.cache_errors
-                script_path.cache_errors.each do |error|
-                  print_error error
-                end
-              end
-            end
-          end
+              puts "SCM: #{repo.scm}" if repo.scm
 
-          #
-          # Lists installed or added Repositories.
-          #
-          def list
-            unless name
-              print_array Repository.all
-            else
-              repo = begin
-                       [Repository.find(name)]
-                     rescue RepositoryNotFound => e
-                       print_error e.message
-                       exit -1
-                     end
+              if repo.verbose?
+                putc "\n"
 
-              print_title repo.name
+                puts "Title: #{repo.title}" if repo.title
+                puts "URI: #{repo.uri}" if repo.uri
+                puts "Source URI: #{repo.source}" if repo.source
+                puts "Website: #{repo.website}" if repo.website
 
-              indent do
-                if repo.installed?
-                  puts "Domain: #{repo.domain}"
-                else
-                  puts "Path: #{repo.path}"
+                executables = repo.executables
+
+                unless executables.empty?
+                  puts "Executables: #{executables.join(', ')}"
                 end
 
-                puts "SCM: #{repo.scm}" if repo.scm
+                putc "\n"
 
-                if repo.verbose?
-                  putc "\n"
+                unless repo.script_paths.empty?
+                  print_title 'Cached Files'
 
-                  puts "Title: #{repo.title}" if repo.title
-                  puts "URI: #{repo.uri}" if repo.uri
-                  puts "Source URI: #{repo.source}" if repo.source
-                  puts "Website: #{repo.website}" if repo.website
-
-                  executables = repo.executables
-
-                  unless executables.empty?
-                    puts "Executables: #{executables.join(', ')}"
-                  end
-
-                  putc "\n"
-
-                  unless repo.script_paths.empty?
-                    print_title 'Cached Files'
-
-                    indent do
-                      repo.script_paths.each do |script_path|
-                        puts script_path.path
-                      end
+                  indent do
+                    repo.script_paths.each do |script_path|
+                      puts script_path.path
                     end
                   end
+                end
 
-                  if repo.description
-                    print_title "Description"
+                if repo.description
+                  print_title "Description"
 
-                    indent { puts "#{repo.description}\n\n" }
-                  else
-                    putc "\n"
-                  end
+                  indent { puts "#{repo.description}\n\n" }
                 else
                   putc "\n"
                 end
+              else
+                putc "\n"
               end
             end
-          end
-
-          #
-          # Adds a Repository.
-          #
-          # @param [String] path
-          #   The path of the local repository.
-          #
-          def add(path)
-            repo = begin
-                     Repository.add(:path => path, :scm => @scm)
-                   rescue DuplicateRepository => e
-                     print_error e.message
-                     exit -1
-                   end
-
-            print_info "Repository #{repo} added."
-          end
-
-          #
-          # Installs a Repository.
-          #
-          # @param [String, URI] uri
-          #   The URI of the remote repository.
-          #
-          def install(uri)
-            repo = begin
-                     Repository.install(:uri => uri, :scm => scm)
-                   rescue DuplicateRepository => e
-                     print_error e.message
-                     exit -1
-                   end
-
-            print_cache_errors(repo)
-            print_info "Repository #{repo} has been installed."
-          end
-
-          #
-          # Updates installed Repositories.
-          #
-          def update
-            repos = if name
-                      begin
-                        [Repository.find(name)]
-                      rescue RepositoryNotFound => e
-                        print_error e.message
-                        exit -1
-                      end
-                    else
-                      Repository.all
-                    end
-
-            repos.each do |repo|
-              print_info "Updating repository #{repo} ..."
-
-              repo.update!
-
-              print_cache_errors(repo)
-              print_info "Repository updated."
-            end
-          end
-
-          #
-          # Uninstalls a Repository.
-          #
-          # @param [String] name
-          #   The name of the repository.
-          #
-          def uninstall(name)
-            repo = Repository.uninstall(name)
-
-            print_info "Uninstalling repository #{repo} ..."
           end
 
         end
