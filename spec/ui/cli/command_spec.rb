1348ca25e3b78dd236a022ed99dc18c4a69ad856
Postmodern <postmodern.mod3@gmail.com>
2012-05-07 20:01:02 -0700

    Added more spec coverage for Command DSL methods.

--- spec/ui/cli/command_spec.rb
+++ spec/ui/cli/command_spec.rb
@@ -6,8 +6,183 @@ require 'ui/cli/classes/test_command'
 describe UI::CLI::Command do
   subject { TestCommand }
 
-  it "should have a command name" do
-    subject.command_name.should == 'test_command'
+  describe "command_name" do
+    it "should be derived from the Class name" do
+      subject.command_name.should == 'test_command'
+    end
+  end
+
+  describe "usage" do
+    context "without an argument" do
+      it "should return the set usage" do
+        subject.usage.should == '[options] PATH FILE [..]'
+      end
+    end
+
+    context "with an argument" do
+      let(:expected) { 'FOO' }
+
+      subject { Class.new(described_class) }
+      before  { subject.usage expected }
+
+      it "should set the usage" do
+        subject.usage.should == expected
+      end
+    end
+
+    context "default" do
+      subject { Class.new(described_class).usage }
+
+      it { should == '[options]' }
+    end
+
+    context "inherited" do
+      let(:superclass) { TestCommand }
+
+      subject { Class.new(superclass).usage }
+
+      it "should default to the usage of the superclass" do
+        subject.should == superclass.usage
+      end
+    end
+  end
+
+  describe "summary" do
+    context "without an argument" do
+      it "should return the set summary" do
+        subject.summary.should == 'Tests the default task'
+      end
+    end
+
+    context "with an argument" do
+      let(:expected) { 'Performs foo' }
+
+      subject { Class.new(described_class) }
+      before  { subject.summary expected }
+
+      it "should set the usage" do
+        subject.summary.should == expected
+      end
+    end
+
+    context "default" do
+      subject { Class.new(described_class).summary }
+
+      it { should == nil }
+    end
+
+    context "inherited" do
+      let(:superclass) { TestCommand }
+
+      subject { Class.new(superclass).summary }
+
+      it "should default to the usage of the superclass" do
+        subject.should == superclass.summary
+      end
+    end
+  end
+
+  describe "options" do
+    subject { Class.new(described_class).options }
+
+    context "inherited" do
+      it "should be {} by default" do
+        subject.should == {}
+      end
+    end
+
+    context described_class do
+      subject { described_class.options }
+
+      it "should have a :verbose option" do
+        subject.should have_key(:verbose)
+      end
+
+      it "should have a :quiet option" do
+        subject.should have_key(:quiet)
+      end
+
+      it "should have a :silent option" do
+        subject.should have_key(:silent)
+      end
+
+      it "should have a :color option" do
+        subject.should have_key(:color)
+      end
+    end
+  end
+
+  describe "option" do
+    let(:name) { :foo }
+
+    it "should define an option" do
+      subject.options[name].should be_kind_of(Hash)
+    end
+
+    it "should define a parameter" do
+      subject.should have_param(name)
+    end
+  end
+
+  describe "each_option" do
+    let(:expected) { [:verbose, :quiet, :silent, :color, :foo] }
+
+    it "should iterate over each option" do
+      names = []
+
+      subject.each_option do |name,options|
+        names << name
+      end
+
+      names.should =~ expected
+    end
+  end
+
+  describe "options?" do
+    it "should test if there are any defined options" do
+      subject.options?.should be_true
+    end
+  end
+
+  describe "arguments" do
+    context "inherited" do
+      subject { Class.new(described_class).arguments }
+
+      it { should == [] }
+    end
+  end
+
+  describe "argument" do
+    let(:name) { :foo }
+
+    subject { Class.new(described_class) }
+    before  { subject.argument name      }
+
+    it "should add to arguments" do
+      subject.arguments.should include(name)
+    end
+
+    it "should define a parameter" do
+      subject.should have_param(name)
+    end
+  end
+
+  describe "each_argument" do
+    let(:expected) { [:path, :files] }
+
+    it "should iterate over each option" do
+      names = []
+
+      subject.each_argument { |name| names << name }
+
+      names.should =~ expected
+    end
+  end
+
+  describe "arguments?" do
+    it "should test if there are any defined arguments" do
+      subject.arguments?.should be_true
+    end
   end
 
   describe "#run" do
