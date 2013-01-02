6404ecb31fe60e2f5b34474e53d21aaf18ab4e2b
Postmodern <postmodern.mod3@gmail.com>
2013-01-01 23:53:16 -0800

    Pre-populate the ronin_licenses table (issue #34).

--- lib/ronin/database/migrations/license.rb
+++ lib/ronin/database/migrations/license.rb
@@ -41,6 +41,92 @@ module Ronin
           drop_table :ronin_licenses
         end
       end
+
+      migration :populate_licenses_table do
+        up do
+          [
+            [
+              'GPL-2',
+              'GNU Public License v2.0',
+              'http://www.gnu.org/licenses/gpl-2.0.txt'
+            ],
+
+            [
+              'GPL-3',
+              'GNU Public License v3.0',
+              'http://www.gnu.org/licenses/gpl-3.0.txt'
+            ],
+
+            [
+              'LGPL-3',
+              'GNU Lesser General Public License v3.0',
+              'http://www.gnu.org/licenses/lgpl-3.0.txt'
+            ],
+
+            [
+              'BSD',
+              'Berkeley Software Distribution License',
+              'http://www.opensource.org/licenses/bsd-license.php'
+            ],
+
+            [
+              'MIT',
+              'The MIT Licence',
+              'http://www.opensource.org/licenses/mit-license.php'
+            ],
+
+            [
+              'CC by',
+              'Creative Commons Attribution v3.0 License',
+              'http://creativecommons.org/licenses/by/3.0/'
+            ],
+
+            [
+              'CC by-sa',
+              'Creative Commons Attribution-Share Alike v3.0 License',
+              'http://creativecommons.org/licenses/by-sa/3.0/'
+            ],
+
+            [
+              'CC by-nd',
+              'Creative Commons Attribution-No Derivative Works v3.0 License',
+              'http://creativecommons.org/licenses/by-nd/3.0/'
+            ],
+
+            [
+              'CC by-nc',
+              'Creative Commons Attribution-Noncommercial v3.0 License',
+              'http://creativecommons.org/licenses/by-nc/3.0/'
+            ],
+
+            [
+              'CC by-nc-sa',
+              'Creative Commons Attribution-Noncommercial-Share Alike v3.0 License',
+              'http://creativecommons.org/licenses/by-nc-sa/3.0/'
+            ],
+
+            [
+              'CC by-nc-nd',
+              'Creative Commons Attribution-Noncommercial-No Derivative Works v3.0 License',
+              'http://creativecommons.org/licenses/by-nc-nd/3.0/'
+            ],
+
+            [
+              'CC0',
+              'Creative Commons Zero License',
+              'http://creativecommons.org/licenses/zero/1.0/'
+            ]
+          ].each do |name,description,url|
+            adapter.execute(
+              'INSERT OR IGNORE INTO ronin_licenses (name,description,url) VALUES (?,?,?)',
+              name, description, url
+            )
+          end
+        end
+
+        down do
+        end
+      end
     end
   end
 end
