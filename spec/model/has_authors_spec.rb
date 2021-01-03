82e3ae214636d7c39d399507e7dbdad5550f28b9
Postmodern <postmodern.mod3@gmail.com>
2021-01-03 00:59:06 -0800

    Refactored Model::HasAuthors specs.

--- spec/model/has_authors_spec.rb
+++ spec/model/has_authors_spec.rb
@@ -4,20 +4,11 @@ require 'model/models/authored_model'
 require 'ronin/model/has_authors'
 
 describe Model::HasAuthors do
-  subject { AuthoredModel }
-
-  before(:all) do
-    resource = subject.new(:content => 'Test')
-    resource.author(
-      :name => 'Alice',
-      :email => 'alice@example.com',
-      :organization => 'Crew'
-    )
-
-    resource.save
-  end
+  let(:model) { AuthoredModel }
 
   describe ".included" do
+    subject { model }
+
     it "should include Ronin::Model" do
       expect(subject.ancestors).to include(Model)
     end
@@ -37,28 +28,52 @@ describe Model::HasAuthors do
     end
   end
 
-  describe "#author" do
-    it "should allow adding authors to a resource" do
-      resource = subject.new
-      resource.author(:name => 'Alice', :email => 'alice@example.com')
+  let(:author_name) { 'Alice' }
+  let(:author_email) { 'alice@example.com' }
+  let(:organization) { 'Org' }
 
-      expect(resource.authors).not_to be_empty
+  describe "#author" do
+    subject do
+      model.new.tap do |resource|
+        resource.author(:name => author_name, :email => author_email)
+      end
+    end
+
+    it "should allow adding authors to a resource" do
+      expect(subject.authors).not_to be_empty
+      expect(subject.authors[0].name).to be(author_name)
+      expect(subject.authors[0].email).to be(author_email)
     end
   end
 
   describe ".written_by" do
+    subject { model }
+
+    before do
+      resource = subject.new(:content => 'Test')
+      resource.author(
+        :name => author_name,
+        :email => author_email,
+        :organization => organization
+      )
+
+      resource.save
+    end
+
     it "should allow querying resources based on their Author" do
-      resources = subject.written_by('Alice')
+      resources = subject.written_by(author_name)
 
       expect(resources.length).to eq(1)
-      expect(resources[0].authors[0].name).to eq('Alice')
+      expect(resources[0].authors[0].name).to be == author_name
     end
 
     it "should allow querying resources based on their Organization" do
-      resources = subject.written_for('Crew')
+      resources = subject.written_for(organization)
 
       expect(resources.length).to eq(1)
-      expect(resources[0].authors[0].organization).to eq('Crew')
+      expect(resources[0].authors[0].organization).to be == organization
     end
+
+    after { subject.destroy }
   end
 end
