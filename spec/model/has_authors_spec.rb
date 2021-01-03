2f0132b61b9dda943bb1099d2b0ae1e16e4559c1
Postmodern <postmodern.mod3@gmail.com>
2021-01-03 00:52:13 -0800

    Use more describes.

--- spec/model/has_authors_spec.rb
+++ spec/model/has_authors_spec.rb
@@ -7,8 +7,6 @@ describe Model::HasAuthors do
   subject { AuthoredModel }
 
   before(:all) do
-    subject.auto_migrate!
-
     resource = subject.new(:content => 'Test')
     resource.author(
       :name => 'Alice',
@@ -19,42 +17,48 @@ describe Model::HasAuthors do
     resource.save
   end
 
-  it "should include Ronin::Model" do
-    expect(subject.ancestors).to include(Model)
+  describe ".included" do
+    it "should include Ronin::Model" do
+      expect(subject.ancestors).to include(Model)
+    end
+
+    it "should define an authors relationship" do
+      relationship = subject.relationships['authors']
+
+      expect(relationship).not_to be_nil
+      expect(relationship.child_model).to eq(Author)
+    end
+
+    it "should define relationships with Author" do
+      relationship = Author.relationships['authored_models']
+
+      expect(relationship).not_to be_nil
+      expect(relationship.child_model).to eq(subject)
+    end
   end
 
-  it "should define an authors relationship" do
-    relationship = subject.relationships['authors']
+  describe "#author" do
+    it "should allow adding authors to a resource" do
+      resource = subject.new
+      resource.author(:name => 'Alice', :email => 'alice@example.com')
 
-    expect(relationship).not_to be_nil
-    expect(relationship.child_model).to eq(Author)
+      expect(resource.authors).not_to be_empty
+    end
   end
 
-  it "should define relationships with Author" do
-    relationship = Author.relationships['authored_models']
-    
-    expect(relationship).not_to be_nil
-    expect(relationship.child_model).to eq(subject)
-  end
+  describe ".written_by" do
+    it "should allow querying resources based on their Author" do
+      resources = subject.written_by('Alice')
 
-  it "should allow adding authors to a resource" do
-    resource = subject.new
-    resource.author(:name => 'Alice', :email => 'alice@example.com')
+      expect(resources.length).to eq(1)
+      expect(resources[0].authors[0].name).to eq('Alice')
+    end
 
-    expect(resource.authors).not_to be_empty
-  end
+    it "should allow querying resources based on their Organization" do
+      resources = subject.written_for('Crew')
 
-  it "should allow querying resources based on their Author" do
-    resources = subject.written_by('Alice')
-
-    expect(resources.length).to eq(1)
-    expect(resources[0].authors[0].name).to eq('Alice')
-  end
-
-  it "should allow querying resources based on their Organization" do
-    resources = subject.written_for('Crew')
-
-    expect(resources.length).to eq(1)
-    expect(resources[0].authors[0].organization).to eq('Crew')
+      expect(resources.length).to eq(1)
+      expect(resources[0].authors[0].organization).to eq('Crew')
+    end
   end
 end
