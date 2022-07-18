a0d1a11ddf3534149d25e6c54bc7fac29ac855ea
Postmodern <postmodern.mod3@gmail.com>
2022-07-18 00:48:43 -0700

    Added the `ronin sha` sub-command (closes #102).

---
+++ lib/ronin/cli/commands/sha1.rb
@@ -0,0 +1,59 @@
+#
+# Copyright (c) 2006-2022 Hal Brodigan (postmodern.mod3 at gmail.com)
+#
+# This file is part of ronin.
+#
+# Ronin is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Ronin is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Ronin.  If not, see <https://www.gnu.org/licenses/>.
+#
+
+require 'ronin/cli/string_command'
+
+require 'digest'
+
+module Ronin
+  class CLI
+    module Commands
+      #
+      # Calculates SHA1 hashes of data.
+      #
+      # ## Usage
+      #
+      #     ronin sha1 [options] [STRING ... | -i FILE]
+      #
+      # ## Options
+      #
+      #     -i, --input FILE                 Optional input file
+      #     -o, --output FILE                Optional output file
+      #     -M, --multiline                  Process each line separately
+      #     -n, --keep-newlines              Preserves newlines at the end of each line
+      #     -h, --help                       Print help information
+      #
+      # ## Arguments
+      #
+      #     [STRING ...]                     Optional string value(s) to process
+      #
+      class Sha1 < StringCommand
+
+        description "Calculates SHA1 hashes of data"
+
+        man_page 'ronin-sha1.1'
+
+        def process_string(string)
+          Digest::SHA1.hexdigest(string)
+        end
+
+      end
+    end
+  end
+end
---
+++ man/ronin-sha1.1.md
@@ -0,0 +1,40 @@
+# ronin-sha1 1 "May 2022" Ronin "User Manuals"
+
+## SYNOPSIS
+
+`ronin sha1` [*options*] [*STRING* ... \| `-i` *FILE*]
+
+## DESCRIPTION
+
+Calculates SHA1 hashes of data.
+
+## ARGUMENTS
+
+*STRING*
+  The optional input string value to hash. If no *STRING* values are given,
+  input will be read from `-i`,`--input` *FILE* or from `stdin`.
+
+## OPTIONS
+
+`-i`, `--input` *FILE*
+  Optional input file to read from.
+
+`-o`, `--output` *FILE*
+  Optional output file to write encoded values to.
+
+`-M`, `--multiline`
+  Process each line of input separately.
+
+`-n`, `--keep-newlines`
+  Preserves newlines at the end of each line.
+
+`-h`, `--help`
+  Print help information.
+
+## AUTHOR
+
+Postmodern <postmodern.mod3@gmail.com>
+
+## SEE ALSO
+
+ronin-md5(1), ronin-sha256(1), ronin-sha512(1)
