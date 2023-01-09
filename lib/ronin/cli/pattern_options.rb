900a60c5f58475d09acc9d80ca240577cd137a53
Postmodern <postmodern.mod3@gmail.com>
2023-01-08 23:36:58 -0800

    Fixed typo in the `-A,--api-key` option description.

--- lib/ronin/cli/pattern_options.rb
+++ lib/ronin/cli/pattern_options.rb
@@ -75,7 +75,7 @@ module Ronin
     #         --public-key                 Searches for all public key data
     #         --aws-access-key-id          Searches for all AWS access key IDs
     #         --aws-secret-access-key      Searches for all AWS secret access key
-    #     -A, --api-key                    Secretes for all API keys
+    #     -A, --api-key                    Searches for all API keys
     #         --single-quoted-string       Searches for all single-quoted strings
     #         --double-quoted-string       Searches for all double-quoted strings
     #     -S, --string                     Searches for all quoted strings
@@ -444,7 +444,7 @@ module Ronin
         end
 
         command.option :api_key, short: '-A',
-                                 desc: 'Secretes for all API keys' do
+                                 desc: 'Searches for all API keys' do
                                    @pattern = API_KEY
                                  end
       end
