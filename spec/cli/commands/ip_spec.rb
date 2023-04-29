56bec0567d0c3ea29034b79b1e4cc1c90b352f60
Christopher M. Hobbs <cmhobbs@acm.org>
2023-04-28 21:52:04 -0500

    Fix formatting and bugs in IP spec.

--- spec/cli/commands/ip_spec.rb
+++ spec/cli/commands/ip_spec.rb
@@ -8,58 +8,66 @@ describe Ronin::CLI::Commands::Ip do
   let(:localhost) { "127.0.0.1" }
 
   describe "#process_value" do
-    describe "when -r option is given" do
+    context "when -r option is given" do
+      before { subject.options[:reverse] = true }
+
       it "will reverse an IPv4 address" do
-        subject.options[:reverse] = true
         expect{ subject.process_value(localhost) }.to output("1.0.0.127.in-addr.arpa\n").to_stdout
       end
     end
 
-    describe "when --hex option is given" do
+    context "when --hex option is given" do
+      before { subject.options[:hex] = true }
+
       it "will convert an IPv4 address to hexadecimal" do
-        subject.options[:hex] = true
         expect{ subject.process_value(localhost) }.to output("0x7f000001\n").to_stdout
       end
     end
 
-    describe "when --decimal option is given" do
+    context "when --decimal option is given" do
+      before { subject.options[:decimal] = true }
+
       it "will convert an IPv4 address to decimal" do
-        subject.options[:decimal] = true
         expect{ subject.process_value(localhost) }.to output("2130706433\n").to_stdout
       end
     end
 
-    describe "when --octal option is given" do
+    context "when --octal option is given" do
+      before { subject.options[:octal] = true }
+
       it "will convert an IPv4 address to octal" do
-        subject.options[:octal] = true
         expect{ subject.process_value(localhost) }.to output("017700000001\n").to_stdout
       end
     end
 
-    describe "when --binary option is given" do
+    context "when --binary option is given" do
+      before { subject.options[:binary] = true }
+
       it "will convert an IPv4 address to binary" do
-        subject.options[:binary] = true
         expect{ subject.process_value(localhost) }.to output("1111111000000000000000000000001\n").to_stdout
       end
     end
 
-    describe "when --octal-octet option is given" do
+    context "when --octal-octet option is given" do
+      before { subject.options[:octal_octet] = true }
+
       it "will convert an IPv4 address to octal by octet" do
-        subject.options[:octal_octet] = true
         expect{ subject.process_value(localhost) }.to output("0177.00.00.01\n").to_stdout
       end
     end
 
-    describe "when --hex-octet option is given" do
+    context "when --hex-octet option is given" do
+      before { subject.options[:hex_octet] = true }
+
       it "will convert an IPv4 address to hex by octet" do
-        subject.options[:hex_octet] = true
         expect{ subject.process_value(localhost) }.to output("7f.0.0.1\n").to_stdout
       end
     end
 
-    describe "when --ipv6-compat option is given" do
+    context "when --ipv6-compat option is given" do
+      before { subject.options[:ipv6_compat] = true }
+
       it "will convert an IPv4 address to an IPv6 compatible address" do
-        subject.options[:ipv6_compat] = true
         expect{ subject.process_value(localhost) }.to output("::ffff:127.0.0.1\n").to_stdout
       end
     end
@@ -67,52 +75,61 @@ describe Ronin::CLI::Commands::Ip do
   end
 
   describe "#format_ip" do
-    describe "when options[:hex] is present" do
+    let(:ip) { Ronin::Support::Network::IP.new(localhost) }
+
+    context "when options[:hex] is present" do
+      before { subject.options[:hex] = true }
+
       it "will convert an IPv4 address to hexadecimal" do
-        subject.options[:hex] = true
-        expect(subject.format_ip(localhost)).to eq("0x7f")
+        expect(subject.format_ip(ip)).to eq("0x7f000001")
       end
     end
 
-    describe "when options[:decimal] is present" do
+    context "when options[:decimal] is present" do
+      before { subject.options[:decimal] = true }
+
       it "will convert an IPv4 address to decimal" do
-        subject.options[:decimal] = true
-        expect(subject.format_ip(localhost)).to eq("127")
+        expect(subject.format_ip(ip)).to eq("2130706433")
       end
     end
 
-    describe "when options[:octal] is present" do
+    context "when options[:octal] is present" do
+      before { subject.options[:octal] = true }
+      
       it "will convert an IPv4 address to octal" do
-        subject.options[:octal] = true
-        expect(subject.format_ip(localhost)).to eq("0177")
+        expect(subject.format_ip(ip)).to eq("017700000001")
       end
     end
 
-    describe "when options[:binary] is present" do
+    context "when options[:binary] is present" do
+      before { subject.options[:binary] = true }
+
       it "will convert an IPv4 address to binary" do
-        subject.options[:binary] = true
-        expect(subject.format_ip(localhost)).to eq("1111111")
+        expect(subject.format_ip(ip)).to eq("1111111000000000000000000000001")
       end
     end
 
-    describe "when options[:octal_octet] is present" do
+    context "when options[:octal_octet] is present" do
+      before { subject.options[:octal_octet] = true }
+
       it "will convert an IPv4 address to octal by octet" do
-        subject.options[:octal_octet] = true
-        expect(subject.format_ip(localhost)).to eq("0177.00.00.01")
+        expect(subject.format_ip(ip)).to eq("0177.00.00.01")
       end
     end
 
-    describe "when options[:hex_octet] is present" do
+    context "when options[:hex_octet] is present" do
+      before { subject.options[:hex_octet] = true }
+
       it "will convert an IPv4 address to hex by octet" do
-        subject.options[:hex_octet] = true
-        expect(subject.format_ip(localhost)).to eq("7f.0.0.1")
+        expect(subject.format_ip(ip)).to eq("7f.0.0.1")
       end
     end
 
-    describe "when options[:ipv6_compat] is present" do
+    context "when options[:ipv6_compat] is present" do
+      before { subject.options[:ipv6_compat] = true }
+
       it "will convert an IPv4 address to an IPv6 compatible address" do
-        subject.options[:ipv6_compat] = true
-        expect(subject.format_ip(localhost)).to eq("::ffff:127.0.0.1")
+        expect(subject.format_ip(ip)).to eq("::ffff:127.0.0.1")
       end
     end
 
