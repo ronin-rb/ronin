609cd83a94d5b55b49379bd071c93b2aaaafb32e
Postmodern <postmodern.mod3@gmail.com>
2023-09-29 16:01:39 -0700

    Add missing require for `ronin/support/crypto/key` (closes #170).

--- lib/ronin/cli/commands/cert_gen.rb
+++ lib/ronin/cli/commands/cert_gen.rb
@@ -18,6 +18,7 @@
 
 require 'ronin/cli/command'
 require 'ronin/support/crypto/cert'
+require 'ronin/support/crypto/key'
 require 'ronin/support/crypto/key/rsa'
 require 'ronin/support/crypto/key/ec'
 require 'ronin/support/text/patterns'
