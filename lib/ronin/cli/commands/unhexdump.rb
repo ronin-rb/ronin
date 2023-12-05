9bdefad7fd0530d3ef6458e01fd3cef3d2bc52d2
Postmodern <postmodern.mod3@gmail.com>
2023-12-04 17:12:39 -0800

    Add missing `description` for the `ronin unhexdump` command.

--- lib/ronin/cli/commands/unhexdump.rb
+++ lib/ronin/cli/commands/unhexdump.rb
@@ -149,6 +149,8 @@ module Ronin
         argument :file, required: false,
                         desc: 'Optional file to unhexdump'
 
+        description 'Decodes a hexdump back into raw data'
+
         man_page 'ronin-unhexdump.1'
 
         #
