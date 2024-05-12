8690ce9220b4a4ec5c2181c452930641bca47b5a
Postmodern <postmodern.mod3@gmail.com>
2024-05-12 14:07:47 -0700

    Added specs for `CLI::Commands::Grep#print_line_prefix`.

--- spec/cli/commands/grep_spec.rb
+++ spec/cli/commands/grep_spec.rb
@@ -14,6 +14,74 @@ describe Ronin::CLI::Commands::Grep do
   let(:reset_intensity) { CommandKit::Colors::ANSI::RESET_INTENSITY }
   let(:reset)           { reset_color + reset_intensity }
 
+  describe "#print_line_prefix" do
+    let(:filename)    { 'foo.txt' }
+    let(:line_number) { 123 }
+
+    let(:stdout) { StringIO.new }
+
+    subject { described_class.new(stdout: stdout) }
+    before { allow(stdout).to receive(:tty?).and_return(true) }
+
+    it "must not print anything by default" do
+      subject.print_line_prefix(filename: filename, line_number: line_number)
+
+      expect(stdout.string).to be_empty
+    end
+
+    context "when the --with-filename option is given" do
+      before { subject.options[:with_filename] = true }
+
+      it "must print the filename in magenta followed by a ':' in cyan" do
+        subject.print_line_prefix(filename: filename, line_number: line_number)
+
+        expect(stdout.string).to eq(
+          "#{magenta}#{filename}#{reset_color}#{cyan}:#{reset_color}"
+        )
+      end
+    end
+
+    context "when the --line-numbers option is given" do
+      before { subject.options[:line_numbers] = true }
+
+      it "must print the line number in green followed by a ':' in cyan" do
+        subject.print_line_prefix(filename: filename, line_number: line_number)
+
+        expect(stdout.string).to eq(
+          "#{green}#{line_number}#{reset_color}#{cyan}:#{reset_color}"
+        )
+      end
+    end
+
+    context "when the --with-filename and the --line-numbers options are given" do
+      before do
+        subject.options[:with_filename] = true
+        subject.options[:line_numbers]  = true
+      end
+
+      it "must print the filename in magenta followed by a ':' in cyan, and the line number in green followed by a ':' in cyan" do
+        subject.print_line_prefix(filename: filename, line_number: line_number)
+
+        expect(stdout.string).to eq(
+          "#{magenta}#{filename}#{reset_color}#{cyan}:#{reset_color}" +
+          "#{green}#{line_number}#{reset_color}#{cyan}:#{reset_color}"
+        )
+      end
+    end
+
+    context "when the --line-numbers option is given" do
+      before { subject.options[:line_numbers] = true }
+
+      it "must print the line number in green followed by a ':' in cyan" do
+        subject.print_line_prefix(filename: filename, line_number: line_number)
+
+        expect(stdout.string).to eq(
+          "#{green}#{line_number}#{reset_color}#{cyan}:#{reset_color}"
+        )
+      end
+    end
+  end
+
   describe "#print_match" do
     let(:text)    { "The quick brown fox jumps over the lazy dog" }
     let(:pattern) { "jumps" }
