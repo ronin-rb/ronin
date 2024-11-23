1f7803e3e906894c609bcf839f48541cf02b5f76
Postmodern <postmodern.mod3@gmail.com>
2024-11-23 13:57:44 -0800

    Added the `--smtp` option to `ronin unescape` (closes #201).

--- spec/cli/commands/unescape_spec.rb
+++ spec/cli/commands/unescape_spec.rb
@@ -140,6 +140,18 @@ describe Ronin::CLI::Commands::Unescape do
       end
     end
 
+    describe "--smtp" do
+      let(:argv) { %w[--smtp] }
+
+      it "must require 'ronin/support/encoding/smtp'" do
+        expect(require('ronin/support/encoding/smtp')).to be(false)
+      end
+
+      it "must add :smtp_unescape to #method_calls" do
+        expect(subject.method_calls.last).to eq(:smtp_unescape)
+      end
+    end
+
     describe "--xml" do
       let(:argv) { %w[--xml] }
 
